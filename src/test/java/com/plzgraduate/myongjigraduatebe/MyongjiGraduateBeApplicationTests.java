7800d71e3cbce9806c88fdab9d53c19957813ca6
dojinyou <djyou128@gmail.com>
2022-09-26 21:14:32 +0900

    [#1]feat: 프로젝트 초기 셋업

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/MyongjiGraduateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plzgraduate.myongjigraduatebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyongjiGraduateBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
