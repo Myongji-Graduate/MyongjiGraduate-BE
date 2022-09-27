db6e05339cbc0b6d4daad474ca8da8eda2a5638b
dojinyou <djyou128@gmail.com>
2022-09-28 00:08:25 +0900

    [#4]chore: test profile 추가

--- src/test/java/com/plzgraduate/myongjigraduatebe/MyongjiGraduateBeApplicationTests.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/MyongjiGraduateBeApplicationTests.java
@@ -2,7 +2,10 @@ package com.plzgraduate.myongjigraduatebe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class MyongjiGraduateBeApplicationTests {
 
