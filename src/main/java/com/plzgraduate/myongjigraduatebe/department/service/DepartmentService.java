a830ea0c20f1db8a233fa342ebc1873d00ea336e
dojinyou <djyou128@gmail.com>
2022-09-30 04:20:29 +0900

    [#11]feat: 학과 도메인 컨트롤러 정의

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/department/service/DepartmentService.java
@@ -0,0 +1,8 @@
+package com.plzgraduate.myongjigraduatebe.department.service;
+
+import com.plzgraduate.myongjigraduatebe.department.service.dto.AllDepartmentsResponse;
+
+public interface DepartmentService {
+
+  AllDepartmentsResponse findAll();
+}
