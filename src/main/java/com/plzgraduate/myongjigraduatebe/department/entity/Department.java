87c65e11fc9abc64b700d25992d9c7f1c267a550
dojinyou <djyou128@gmail.com>
2022-10-01 21:16:03 +0900

    [#13]feat: 지원 학번 정보 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/department/entity/Department.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/department/entity/Department.java
@@ -3,6 +3,7 @@ package com.plzgraduate.myongjigraduatebe.department.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 import com.plzgraduate.myongjigraduatebe.common.entity.BaseEntity;
 
@@ -15,11 +16,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Department extends BaseEntity {
 
+  public static final int DEFAULT_ENDED_ENTRY_YEAR = 99;
   @NotNull
   @Column()
   private String name;
 
-  public Department(String name) {
+  @NotNull
+  @Positive
+  @Column(columnDefinition = "UNSIGNED TINYINT", nullable = false)
+  private int startedEntryYear;
+
+  @NotNull
+  @Positive
+  @Column(columnDefinition = "UNSIGNED TINYINT")
+  private int endedEntryYear;
+
+  public Department(
+      String name,
+      int startedEntryYear
+  ) {
+    this(name, startedEntryYear, DEFAULT_ENDED_ENTRY_YEAR);
+  }
+
+  public Department(
+      String name,
+      int startedEntryYear,
+      int endedEntryYear
+  ) {
     this.name = name;
+    this.startedEntryYear = startedEntryYear;
+    this.endedEntryYear = endedEntryYear;
+  }
+
+  public boolean isSupported(int entryYear) {
+    return startedEntryYear <= entryYear && entryYear <= endedEntryYear;
   }
 }
