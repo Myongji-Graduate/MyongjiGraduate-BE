b60f98076aaebc53cfcd5dac7f3c552286cec360
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-04 20:12:43 +0900

    [#27]refactor: 충돌 해결

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/controller/GraduationApiController.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/controller/GraduationApiController.java
@@ -3,10 +3,13 @@ package com.plzgraduate.myongjigraduatebe.graduation.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.plzgraduate.myongjigraduatebe.common.utils.PdfParser;
+import com.plzgraduate.myongjigraduatebe.graduation.dto.GraduationRequest;
 import com.plzgraduate.myongjigraduatebe.graduation.dto.GraduationResult;
 import com.plzgraduate.myongjigraduatebe.graduation.service.GraduationService;
 
@@ -19,10 +22,14 @@ public class GraduationApiController {
 
   private final GraduationService graduationService;
 
-  @PostMapping("test")
+  @PostMapping("result")
   @ResponseStatus(HttpStatus.OK)
-  public GraduationResult testAssess() {
-    return graduationService.assess(PdfParser.getSampleString());
+  public String access(
+      @RequestParam String entryYear,
+      @RequestParam String department,
+      @RequestParam MultipartFile file
+  ) {
+    GraduationRequest request = new GraduationRequest(entryYear, department, file);
+    return PdfParser.parseString(request);
   }
-
 }
