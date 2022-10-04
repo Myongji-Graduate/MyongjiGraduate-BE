f52f98084e664bd95f7037d9f11f98741fa0e9f6
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-04 16:06:50 +0900

    [#27]feat: request 객체 정의

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/dto/GraduationRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/dto/GraduationRequest.java
@@ -1,8 +1,27 @@
 package com.plzgraduate.myongjigraduatebe.graduation.dto;
 
+import java.util.regex.Pattern;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Getter;
 
 @Getter
 public class GraduationRequest {
 
+  private static final int startEntryYear = 16;
+  private static final int endEntryYear = 22;
+  private final String entryYear;
+  private final String department;
+  private final MultipartFile file;
+
+  public GraduationRequest(
+      String entryYear,
+      String department,
+      MultipartFile file
+  ) {
+    this.entryYear = entryYear;
+    this.department = department;
+    this.file = file;
+  }
 }
