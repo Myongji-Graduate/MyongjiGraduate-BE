355d12ae81e11eadb9c6889cb4892453513272a1
JEONG-JIWHAN <jjw981210@gmail.com>
2022-10-04 20:09:11 +0900

    [#27]feat: request 검증 로직 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/dto/GraduationRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/dto/GraduationRequest.java
@@ -20,8 +20,39 @@ public class GraduationRequest {
       String department,
       MultipartFile file
   ) {
+    validate(entryYear, department, file);
     this.entryYear = entryYear;
     this.department = department;
     this.file = file;
+  }
+
+  private void validate(
+      String entryYear,
+      String department,
+      MultipartFile file
+  ) {
+    if (entryYear.isEmpty()) {
+      throw new IllegalArgumentException("입학년도를 입력해주세요");
+    }
+    validateEntryYear(entryYear);
+    if (department.isEmpty()) {
+      throw new IllegalArgumentException("학과을 선택해주세요");
+    }
+    if (file.isEmpty()) {
+      throw new IllegalArgumentException("PDF를 찾을 수 없습니다");
+    }
+  }
+
+  private void validateEntryYear(String entryYear) {
+    String pattern = "^\\d*$";
+    if (!Pattern.matches(pattern, entryYear)) {
+      throw new IllegalArgumentException("문자가 포함되어 있습니다");
+    }
+    if (entryYear.length() != 2) {
+      throw new IllegalArgumentException("16~22 사이를 입력해주세요");
+    }
+    if (Integer.parseInt(entryYear) < startEntryYear || Integer.parseInt(entryYear) > endEntryYear) {
+      throw new IllegalArgumentException("16~22 사이를 입력해주세요");
+    }
   }
 }
