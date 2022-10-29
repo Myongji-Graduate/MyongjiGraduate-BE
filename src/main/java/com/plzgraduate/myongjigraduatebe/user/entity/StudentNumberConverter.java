2709443673af8811ddbb7c217878785a257f6ab1
dojinyou <djyou128@gmail.com>
2022-10-29 11:12:37 +0900

    [#55]feat: 사용자 엔티티 정의

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/entity/StudentNumberConverter.java
@@ -0,0 +1,17 @@
+package com.plzgraduate.myongjigraduatebe.user.entity;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class StudentNumberConverter implements AttributeConverter<StudentNumber, String> {
+  @Override
+  public String convertToDatabaseColumn(StudentNumber studentNumber) {
+    return studentNumber.getValue();
+  }
+
+  @Override
+  public StudentNumber convertToEntityAttribute(String dbData) {
+    return StudentNumber.of(dbData);
+  }
+}
