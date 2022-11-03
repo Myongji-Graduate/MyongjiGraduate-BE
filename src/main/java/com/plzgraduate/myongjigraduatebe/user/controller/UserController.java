0c99dc00296033c9fca9945c2f7c92dd9d917eff
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-03 15:22:36 +0900

    [#60]feat: 수강과목 불러오는 api 생성 & 기준 엔드포인트 수정

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/controller/UserController.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.plzgraduate.myongjigraduatebe.user.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,26 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.plzgraduate.myongjigraduatebe.user.dto.TakenLectureResponse;
 import com.plzgraduate.myongjigraduatebe.user.service.TakenLectureService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/user")
+@RequestMapping("api/v1/users")
 public class UserController {
 
   private final TakenLectureService takenLectureService;
 
-  @PostMapping("/{id}/taken-lecture")
+  @GetMapping("/{id}/taken-lectures")
+  @ResponseStatus(HttpStatus.OK)
+  public TakenLectureResponse show(@PathVariable("id") Long id){
+    return takenLectureService.showTakenLecture(id);
+  }
+
+  @PostMapping("/{id}/taken-lectures")
   @ResponseStatus(HttpStatus.OK)
   public String save(
-      @PathVariable("id") Long userId,
+      @PathVariable("id") Long id,
       @RequestBody String parsingText
   ) {
     String[] userText = parsingText.split(", ");
     String[] lectureText = parsingText.split(", ")[27].split(",");
-    takenLectureService.saveTakenLecture(userId, lectureText);
+    takenLectureService.saveTakenLecture(id, lectureText);
     return "redirect:/{id}/taken-lecture";
   }
 
