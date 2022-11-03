5f51310f56190ebfa7d5a572180af029a0aaa23a
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-03 15:17:48 +0900

    [#60]feat: 데이터 쿼리문 작성

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/service/TakenLectureService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/service/TakenLectureService.java
@@ -1,8 +1,13 @@
 package com.plzgraduate.myongjigraduatebe.user.service;
 
+import com.plzgraduate.myongjigraduatebe.user.dto.TakenLectureResponse;
+
 public interface TakenLectureService {
   void saveTakenLecture(
       Long userId,
       String[] lectureText
   );
+
+  TakenLectureResponse showTakenLecture(Long userId);
+
 }
