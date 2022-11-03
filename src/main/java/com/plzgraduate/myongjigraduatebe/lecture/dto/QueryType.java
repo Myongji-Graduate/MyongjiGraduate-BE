5064808a76078e3831f2ab01edcb13a3a04e650e
5uhwann <5uhwann27@gmail.com>
2022-11-04 01:03:47 +0900

    [#61]refactor: LectureApiController 쿼리파라미터 dto 변환

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/dto/QueryType.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/dto/QueryType.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum QueryType {
-  LECTURE_NAME("과목명"),
-  LECTURE_CODE("과목코드");
-
-  private final String qType;
+  NAME,
+  CODE
 }
--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/service/LectureService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/service/LectureService.java
@@ -1,10 +1,11 @@
 package com.plzgraduate.myongjigraduatebe.lecture.service;
 
 import com.plzgraduate.myongjigraduatebe.lecture.dto.AllSearchedLecturesResponse;
+import com.plzgraduate.myongjigraduatebe.lecture.dto.QueryType;
 
 public interface LectureService {
   AllSearchedLecturesResponse searchLecture(
       String keyword,
-      String qType
+      QueryType qType
   );
 }
