104329a9ebf8b99ada50d05511ba5e86da1f6376
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-04 06:06:00 +0900

    [#70]feat: db에서 수강과목 삭제하기

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/repository/TakenLectureRepository.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/repository/TakenLectureRepository.java
@@ -1,16 +1,23 @@
 package com.plzgraduate.myongjigraduatebe.user.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.plzgraduate.myongjigraduatebe.lecture.entity.Lecture;
 import com.plzgraduate.myongjigraduatebe.user.entity.TakenLecture;
 import com.plzgraduate.myongjigraduatebe.user.entity.User;
 
 public interface TakenLectureRepository extends JpaRepository<TakenLecture, Long> {
   @Query("select distinct t from TakenLecture t join fetch t.lecture where t.user = :user")
   List<TakenLecture> findAllByUserWithFetchJoin(@Param("user") User user);
+
+  @Transactional
+  void deleteTakenLectureByUserAndLecture(
+      User user,
+      Lecture lecture
+  );
 }
