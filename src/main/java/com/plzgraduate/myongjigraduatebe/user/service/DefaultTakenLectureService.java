52e80ac6a5fc957e75c4c3c3365584cc341bac01
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-03 15:15:16 +0900

    [#60]feat: 수강과목 불러오는 기능 구현 & 파싱데이터 변환 로직 수정

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/service/DefaultTakenLectureService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/service/DefaultTakenLectureService.java
@@ -2,12 +2,15 @@ package com.plzgraduate.myongjigraduatebe.user.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.plzgraduate.myongjigraduatebe.lecture.entity.Lecture;
 import com.plzgraduate.myongjigraduatebe.lecture.entity.LectureCode;
 import com.plzgraduate.myongjigraduatebe.lecture.repository.LectureRepository;
+import com.plzgraduate.myongjigraduatebe.user.dto.TakenLectureDto;
+import com.plzgraduate.myongjigraduatebe.user.dto.TakenLectureResponse;
 import com.plzgraduate.myongjigraduatebe.user.entity.TakenLecture;
 import com.plzgraduate.myongjigraduatebe.user.entity.User;
 import com.plzgraduate.myongjigraduatebe.user.repository.TakenLectureRepository;
@@ -24,22 +27,45 @@ public class DefaultTakenLectureService implements TakenLectureService {
 
   @Override
   public void saveTakenLecture(
-      Long userId,
+      Long id,
       String[] lectureText
   ) {
-    User user = userRepository.findUserById(userId);
+    User user = userRepository.findUserById(id);
     List<LectureCode> takenLectureCodes = getTakenLectureCodes(lectureText);
     List<Lecture> lectures = lectureRepository.findAllByLectureCodeIsIn(takenLectureCodes);
     lectures.forEach(lecture -> takenLectureRepository.save(new TakenLecture(user, lecture)));
   }
 
+  @Override
+  public TakenLectureResponse showTakenLecture(Long id) {
+    User user = userRepository.findUserById(id);
+    List<Lecture> lectures =
+        takenLectureRepository.findAllByUserWithFetchJoin(user)
+        .stream()
+        .map(takenLecture -> takenLecture.getLecture())
+        .collect(Collectors.toList());
+    List<TakenLectureDto> takenLectureDtoList = new ArrayList<>();
+    lectures
+        .forEach(lecture -> takenLectureDtoList.add(TakenLectureDto
+                                                        .builder()
+                                                        .code(lecture.getCode())
+                                                        .name(lecture.getName())
+                                                        .credit(lecture.getCredit())
+                                                        .build()));
+    return new TakenLectureResponse(takenLectureDtoList);
+  }
+
   private List<LectureCode> getTakenLectureCodes(
       String[] splitText
   ) {
-    int countTakenLecture = (splitText.length - 9) / 8;
-    List<LectureCode> takenLectureCods = new ArrayList<>(countTakenLecture);
+    List<LectureCode> takenLectureCods = new ArrayList<>();
     for (int i = 9; i < splitText.length; i += 7) {
+      if(Character.isDigit(splitText[i+3].charAt(0))){
+        String code = splitText[i + 3];
+        return takenLectureCods;
+      }
       String code = splitText[i + 3];
+      System.out.println(code);
       if (i + 7 < splitText.length && !splitText[i + 7].isBlank() && Character.isDigit(splitText[i + 7].charAt(0))) {
         i++;
       }
