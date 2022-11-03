1ddf299a6f6b1b269fec56c32a00a0cee06f3cae
5uhwann <5uhwann27@gmail.com>
2022-11-04 02:15:42 +0900

    [#61]refactor: 불필요 어노테이션 삭제

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/dto/QueryType.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/dto/QueryType.java
@@ -1,10 +1,8 @@
 package com.plzgraduate.myongjigraduatebe.lecture.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum QueryType {
   NAME,
   CODE
