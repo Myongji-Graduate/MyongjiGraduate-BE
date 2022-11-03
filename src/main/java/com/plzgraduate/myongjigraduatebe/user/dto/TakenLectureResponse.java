2d245f333a5063c2ca25b648b687f30ed20b3934
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-04 03:41:37 +0900

    [#60]feat: 수강과목 response 객체 수정

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/dto/TakenLectureResponse.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/dto/TakenLectureResponse.java
@@ -8,7 +8,17 @@ import lombok.Getter;
 public class TakenLectureResponse {
   private final List<TakenLectureDto> takenLectureDtoList;
 
-  public TakenLectureResponse(List<TakenLectureDto> takenLectureDtoList) {
+  private TakenLectureResponse(List<TakenLectureDto> takenLectureDtoList) {
     this.takenLectureDtoList = takenLectureDtoList;
   }
+
+  public static TakenLectureResponse of(List<TakenLectureDto> takenLectureDtoList){
+    return new TakenLectureResponse(takenLectureDtoList);
+  }
+
+  public void addTakenLectureDto(TakenLectureDto takenLectureDto){
+    takenLectureDtoList.add(takenLectureDto);
+  }
+
+
 }
