89a75457656a7072c9e579832e0b2b312b946f29
5uhwann <5uhwann27@gmail.com>
2022-11-04 01:18:36 +0900

    [#61]refactor: findByLectureCodeLike 파라미터 변경

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/repository/LectureCustomRepository.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/repository/LectureCustomRepository.java
@@ -3,10 +3,11 @@ package com.plzgraduate.myongjigraduatebe.lecture.repository;
 import java.util.List;
 
 import com.plzgraduate.myongjigraduatebe.lecture.dto.SearchedLecture;
+import com.plzgraduate.myongjigraduatebe.lecture.entity.LectureCode;
 
 public interface LectureCustomRepository {
 
-  List<SearchedLecture> findByLectureCodeLike(String lectureCode);
+  List<SearchedLecture> findByLectureCodeLike(LectureCode lectureCode);
 
   List<SearchedLecture> findByLectureNameBy(String lectureName);
 }
