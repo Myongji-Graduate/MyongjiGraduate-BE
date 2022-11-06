67bf48ca9b18ea30738cd2448f988f97babee778
dojinyou <djyou128@gmail.com>
2022-11-06 10:07:56 +0900

    [#53]feat: 예외 핸들러 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/common/exception/ExceptionController.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/common/exception/ExceptionController.java
@@ -3,6 +3,7 @@ package com.plzgraduate.myongjigraduatebe.common.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,8 +15,15 @@ public class ExceptionController {
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ExceptionResponse handleIllegalArgumentException(IllegalArgumentException e) {
-    logger.warn("IllegalArgumentException : ", e);
+    logger.warn("IllegalArgumentException: ", e);
     return ExceptionResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ExceptionResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    logger.warn("HttpMessageNotReadableException: ", e);
+    return ExceptionResponse.of(HttpStatus.BAD_REQUEST, e.getCause().getCause().getMessage());
   }
 
   @ExceptionHandler(RuntimeException.class)
