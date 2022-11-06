432889732a03f1a787c4d4529432c19519411c57
dojinyou <djyou128@gmail.com>
2022-11-06 11:36:56 +0900

    [#52]feat: 로그인 기능 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/auth/controller/AuthController.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/auth/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.plzgraduate.myongjigraduatebe.auth.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.plzgraduate.myongjigraduatebe.auth.dto.SignInRequest;
 import com.plzgraduate.myongjigraduatebe.auth.dto.SignUpRequest;
 import com.plzgraduate.myongjigraduatebe.auth.service.JwtAuthService;
 
@@ -23,6 +25,16 @@ public class AuthController {
   @ResponseStatus(HttpStatus.CREATED)
   public void signUp(@RequestBody SignUpRequest userCreateReq) {
     authService.signUp(userCreateReq);
+  }
+
+  @PostMapping(value = "sign-in")
+  public ResponseEntity<Void> signIn(@RequestBody SignInRequest request) {
+    String authorizationHeader = authService.signIn(request);
+
+    return ResponseEntity
+        .ok()
+        .header("Authorization", authorizationHeader)
+        .build();
   }
 
 }
--- src/main/java/com/plzgraduate/myongjigraduatebe/auth/service/JwtAuthService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/auth/service/JwtAuthService.java
@@ -1,9 +1,15 @@
 package com.plzgraduate.myongjigraduatebe.auth.service;
 
+import java.util.Date;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.plzgraduate.myongjigraduatebe.auth.dto.SignInRequest;
 import com.plzgraduate.myongjigraduatebe.auth.dto.SignUpRequest;
+import com.plzgraduate.myongjigraduatebe.common.config.JwtConfig;
 import com.plzgraduate.myongjigraduatebe.user.entity.User;
 import com.plzgraduate.myongjigraduatebe.user.repository.UserRepository;
 
@@ -13,8 +19,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAuthService implements AuthService {
 
+  public static final String ERROR_MESSAGE_FOR_SIGN_IN = "아이디 혹은 비밀번호를 확인하세요.";
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JwtConfig jwtConfig;
 
   @Override
   public void signUp(SignUpRequest signUpReq) {
@@ -25,5 +33,42 @@ public class JwtAuthService implements AuthService {
     }
 
     userRepository.save(newUser);
+  }
+
+  @Override
+  public String signIn(SignInRequest request) {
+    User user = userRepository
+        .findByUserId(request.getUserId())
+        .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_FOR_SIGN_IN));
+
+    if (!passwordEncoder.matches(request
+                                     .getPassword()
+                                     .getValue(), user.getPassword())) {
+      throw new IllegalArgumentException(ERROR_MESSAGE_FOR_SIGN_IN);
+    }
+
+    return createAuthorizationHeader(user);
+  }
+
+  private String createAuthorizationHeader(User user) {
+    String jwt = createJwt(user);
+    return String.format("%s %s", jwtConfig.getPrefix(), jwt);
+  }
+
+  private String createJwt(User user) {
+    Date now = new Date();
+    Date expireAt = new Date(now.getTime() + jwtConfig.getExpirySeconds() * 1000L);
+    String[] roles = user
+        .getRoles()
+        .toArray(new String[0]);
+
+    return JWT
+        .create()
+        .withIssuer(jwtConfig.getIssuer())
+        .withIssuedAt(now)
+        .withExpiresAt(expireAt)
+        .withClaim("id", user.getId())
+        .withArrayClaim("roles", roles)
+        .sign(Algorithm.HMAC512(jwtConfig.getClientSecret()));
   }
 }
