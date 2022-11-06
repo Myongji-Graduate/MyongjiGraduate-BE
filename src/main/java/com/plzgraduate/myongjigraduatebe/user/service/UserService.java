162afe81ed28a5a46e5bdd28bbd69656d620b0ec
dojinyou <djyou128@gmail.com>
2022-11-06 18:04:36 +0900

    [#74]feat: userid 중복체크 기능 구현

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/service/UserService.java
@@ -0,0 +1,13 @@
+package com.plzgraduate.myongjigraduatebe.user.service;
+
+import org.springframework.stereotype.Service;
+
+import com.plzgraduate.myongjigraduatebe.user.dto.UserIdValidityResponse;
+import com.plzgraduate.myongjigraduatebe.user.entity.UserId;
+
+@Service
+public interface UserService {
+
+  UserIdValidityResponse checkValidityUserId(UserId userId);
+
+}
