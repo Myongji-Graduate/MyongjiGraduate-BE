1a9faadb95a13cbbd19e741b7e8c9d88997e556c
dojinyou <djyou128@gmail.com>
2022-11-08 03:35:10 +0900

    [#82]test: 학번 중복 기능 테스트 작성

--- src/test/java/com/plzgraduate/myongjigraduatebe/user/service/DefaultUserServiceTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/user/service/DefaultUserServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.plzgraduate.myongjigraduatebe.department.entity.Department;
+import com.plzgraduate.myongjigraduatebe.user.dto.StudentNumberValidityResponse;
 import com.plzgraduate.myongjigraduatebe.user.dto.UserIdValidityResponse;
 import com.plzgraduate.myongjigraduatebe.user.entity.EnglishLevel;
 import com.plzgraduate.myongjigraduatebe.user.entity.StudentNumber;
@@ -93,6 +94,56 @@ class DefaultUserServiceTest {
         // then
         assertThat(response).isNotNull();
         assertThat(response.getUserId()).isEqualTo(notExistUserId);
+        assertThat(response.getIsNotDuplicated()).isTrue();
+      }
+    }
+  }
+
+  @Nested
+  @DisplayName("CheckValidityStudentNumber 메서드는")
+  class DescribeCheckValidityStudentNumber {
+
+    @Nested
+    @DisplayName("이미 있는 학번라면")
+    class ContextWithExistStudentNumber {
+
+      @Test
+      @DisplayName("NotDuplicated이 False인 응답을 반환한다.")
+      void ItReturnResponseWithNotDuplicatedOfFalse() {
+
+        //given
+        StudentNumber existStudentNumber = StudentNumber.valueOf(studentNumber);
+        given(userRepository.existsByStudentNumber(existStudentNumber)).willReturn(true);
+
+        // when
+        StudentNumberValidityResponse response = defaultUserService.checkValidityStudentNumber(existStudentNumber);
+
+        // then
+        assertThat(response).isNotNull();
+        assertThat(response.getStudentNumber()).isEqualTo(existStudentNumber);
+        assertThat(response.getIsNotDuplicated()).isFalse();
+
+      }
+    }
+
+    @Nested
+    @DisplayName("존재하지 않는 학번이라면")
+    class ContextWithNotExistStudentNumber {
+
+      @Test
+      @DisplayName("NotDuplicated이 true 인 응답을 반환한다.")
+      void ItReturnResponseWithNotDuplicatedOfTrue() {
+
+        //given
+        StudentNumber notExistStudentNumber = StudentNumber.valueOf(studentNumber);
+        given(userRepository.existsByStudentNumber(notExistStudentNumber)).willReturn(false);
+
+        // when
+        StudentNumberValidityResponse response = defaultUserService.checkValidityStudentNumber(notExistStudentNumber);
+
+        // then
+        assertThat(response).isNotNull();
+        assertThat(response.getStudentNumber()).isEqualTo(notExistStudentNumber);
         assertThat(response.getIsNotDuplicated()).isTrue();
       }
     }
