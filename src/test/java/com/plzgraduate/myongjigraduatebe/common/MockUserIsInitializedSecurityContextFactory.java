55206070fdab3066f8882f226afd92e67b2e2d9e
dojinyou <djyou128@gmail.com>
2022-11-08 04:33:56 +0900

    [#84]test: 테스트를 위한 인증된 사용자 어노테이션 정의

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/common/MockUserIsInitializedSecurityContextFactory.java
@@ -0,0 +1,44 @@
+package com.plzgraduate.myongjigraduatebe.common;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.plzgraduate.myongjigraduatebe.auth.PrincipalDetails;
+import com.plzgraduate.myongjigraduatebe.department.entity.Department;
+import com.plzgraduate.myongjigraduatebe.user.entity.EnglishLevel;
+import com.plzgraduate.myongjigraduatebe.user.entity.StudentNumber;
+import com.plzgraduate.myongjigraduatebe.user.entity.User;
+import com.plzgraduate.myongjigraduatebe.user.entity.UserId;
+
+public class MockUserIsInitializedSecurityContextFactory implements WithSecurityContextFactory<WithMockUserIsInitialized> {
+  @Override
+  public SecurityContext createSecurityContext(WithMockUserIsInitialized annotation) {
+    final SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+    final Long id = 1L;
+    final UserId userId = UserId.valueOf("testUser");
+    final String password = "testUserPassword";
+    final StudentNumber studentNumber = StudentNumber.valueOf("12345678");
+    final EnglishLevel engLv = EnglishLevel.MIDDLE;
+
+    User user = new User(userId, password, studentNumber, engLv);
+
+    final String userName = "testUserName";
+    final Department department = new Department("testDepartment");
+
+    ReflectionTestUtils.setField(user, "id", id);
+    ReflectionTestUtils.setField(user, "name", userName);
+    ReflectionTestUtils.setField(user, "department", department);
+
+    PrincipalDetails principalDetails = new PrincipalDetails(user);
+    Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails, null, principalDetails.getAuthorities());
+
+    securityContext.setAuthentication(authentication);
+
+    return securityContext;
+  }
+}
