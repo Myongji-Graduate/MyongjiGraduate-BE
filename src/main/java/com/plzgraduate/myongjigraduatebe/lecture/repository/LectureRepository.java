b127142c76a1e14276386e914e3805fe68a84d13
dojinyou <djyou128@gmail.com>
2022-11-14 16:56:20 +0900

    [#89]fix: 중복코드 관련 과목 조회 쿼리 수정

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/repository/LectureRepository.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/repository/LectureRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.plzgraduate.myongjigraduatebe.lecture.entity.Lecture;
 import com.plzgraduate.myongjigraduatebe.lecture.entity.LectureCode;
@@ -17,6 +18,7 @@ public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
   Optional<Lecture> findByLectureCode(LectureCode duplicatedCode);
 
-  List<Lecture> findAllByDuplicatedLectureCodeNotNull();
+  @Query(value = "select l from Lecture l where l.lectureCode in (select l2.duplicatedLectureCode from Lecture l2 where l2.duplicatedLectureCode is not null)")
+  List<Lecture> findAllDuplicatedLectures();
 
 }
