88697adca30dca8c30614f5b9ed56758833dfacb
5uhwann <5uhwann27@gmail.com>
2022-11-23 01:57:42 +0900

    [#125]feat: 전공선택필수 초과 과목 이관 기능 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/dto/DetailGraduationResult.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/dto/DetailGraduationResult.java
@@ -1,15 +1,28 @@
 package com.plzgraduate.myongjigraduatebe.graduation.dto;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import com.plzgraduate.myongjigraduatebe.graduation.entity.GraduationCategory;
 import com.plzgraduate.myongjigraduatebe.graduation.entity.GraduationRequirement;
+import com.plzgraduate.myongjigraduatebe.lecture.dto.LectureResponse;
 
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class DetailGraduationResult {
+
+  private static final String MAJOR_CATEGORY_NAME = GraduationCategory.MAJOR.name();
+  private static final Set<String> MAJOR_DETAIL_CATEGORY_NAME = new HashSet<>() {{
+    add("BUSINESS");
+    add("MANAGEMENT_INFORMATION");
+    add("INTERNATIONAL_TRADE");
+    add("ADMINISTRATION");
+  }};
 
   private final String categoryName;
   private boolean isCompleted;
@@ -31,11 +44,21 @@ public class DetailGraduationResult {
   }
 
   public static DetailGraduationResult createFreeElective(GraduationRequirement requirement) {
-    return new DetailGraduationResult(GraduationCategory.FREE_ELECTIVE.name(), requirement.getFreeElectiveCredit(), 0, null);
+    return new DetailGraduationResult(
+        GraduationCategory.FREE_ELECTIVE.name(),
+        requirement.getFreeElectiveCredit(),
+        0,
+        null
+    );
   }
 
   public static DetailGraduationResult createNormalCulture(GraduationRequirement requirement) {
-    return new DetailGraduationResult(GraduationCategory.NORMAL_CULTURE.name(), requirement.getNormalCultureCredit(), 0, null);
+    return new DetailGraduationResult(
+        GraduationCategory.NORMAL_CULTURE.name(),
+        requirement.getNormalCultureCredit(),
+        0,
+        null
+    );
   }
 
   public void checkComplete() {
@@ -60,6 +83,8 @@ public class DetailGraduationResult {
   public int getLeftCredit() {
     int leftCredit = 0;
 
+    shiftLecture();
+
     if (detailCategory == null) {
       leftCredit = Math.max(0, takenCredit - totalCredit);
     } else {
@@ -74,5 +99,27 @@ public class DetailGraduationResult {
 
   public void addTakenCredit(int takenCredit) {
     this.takenCredit += takenCredit;
+  }
+
+  private void shiftLecture() {
+    if (categoryName.equals(MAJOR_CATEGORY_NAME)) {
+      List<LectureResponse> shiftLectures = new ArrayList<>();
+      DetailCategoryResult mainResult = null;
+
+      for (DetailCategoryResult result : detailCategory) {
+        if (MAJOR_DETAIL_CATEGORY_NAME.contains(result.getDetailCategoryName())) {
+          mainResult = result;
+        }
+
+        while (result.getLeftCredit() > 0) {
+          LectureResponse shiftLecture = result.shiftLecture(result.getLeftCredit());
+          shiftLectures.add(shiftLecture);
+        }
+      }
+
+      if (mainResult != null) {
+        mainResult.shift(shiftLectures);
+      }
+    }
   }
 }
