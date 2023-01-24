04b6d4cacfab25a047b40706f9f0fcd5503862ba
5uhwann <5uhwann27@gmail.com>
2023-01-24 21:01:55 +0900

    [#133]refactor: GraduationRequirementService 테스트 코드 수정

--- src/test/java/com/plzgraduate/myongjigraduatebe/graduation/service/GraduationRequirementServiceImplTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/graduation/service/GraduationRequirementServiceImplTest.java
@@ -3,6 +3,7 @@ package com.plzgraduate.myongjigraduatebe.graduation.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,32 +27,34 @@ class GraduationRequirementServiceImplTest {
   @Mock
   private GraduationRequirementRepository graduationRequirementRepository;
 
-  private final GraduationRequirement graduationRequirement = GraduationRequirement
-      .builder()
-      .commonCultureCredit(10)
-      .coreCultureCredit(10)
-      .department(new Department("법학과"))
-      .engLv(EnglishLevel.ENG12)
-      .freeElectiveCredit(10)
-      .normalCultureCredit(10)
-      .basicAcademicalCultureCredit(10)
-      .totalCredit(60)
-      .build();
-
-  private final BachelorInfoRequest bachelorInfoRequest = new BachelorInfoRequest(19, "법학과");
+  private final BachelorInfoRequest bachelorInfoRequest = new BachelorInfoRequest(EntryYear.of(19), "법학과");
 
   @Test
+  @DisplayName("입학년도와 학과로 졸업요건학점을 조회한다.")
   void findByEntryYearAndDepartmentNameTest() {
-    //give
-    given(graduationRequirementRepository.findByNameAndEntryYear(EntryYear.of(bachelorInfoRequest.getEntryYear())
+    //given
+    GraduationRequirement graduationRequirement = GraduationRequirement
+        .builder()
+        .commonCultureCredit(10)
+        .coreCultureCredit(10)
+        .department(new Department("법학과"))
+        .engLv(EnglishLevel.ENG12)
+        .freeElectiveCredit(10)
+        .normalCultureCredit(10)
+        .basicAcademicalCultureCredit(10)
+        .totalCredit(60)
+        .build();
+
+    given(graduationRequirementRepository.findByNameAndEntryYear(bachelorInfoRequest.getEntryYear()
         , bachelorInfoRequest.getDepartment(), EnglishLevel.ENG12)).willReturn(graduationRequirement);
 
     //when
-    BachelorInfoRequirementResponse bachelorInfoRequirementResponse = graduationRequirementService.find(
+    BachelorInfoRequirementResponse bachelorInfoRequirementResponse = graduationRequirementService.getBachelorInfoRequirement(
         bachelorInfoRequest);
 
     //then
     assertThat(bachelorInfoRequirementResponse).isNotNull();
+    assertThat(bachelorInfoRequirementResponse.getTotalCredit()).isEqualTo(graduationRequirement.getTotalCredit());
   }
 
 }
