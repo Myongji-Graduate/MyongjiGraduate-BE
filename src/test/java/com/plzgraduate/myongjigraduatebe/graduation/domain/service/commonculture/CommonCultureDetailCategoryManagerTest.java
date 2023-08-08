7a76526bdfc995d13233a20ca3af67899e2661ed
5uhwann <5uhwann27@gmail.com>
2023-08-09 00:17:36 +0900

    test: 공통교양 세부 카테고리(16~19 기독교) 졸업 결과 생성 시 필수 과목 체크 로직 테스트 작성

--- src/test/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/commonculture/CommonCultureDetailCategoryManagerTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/commonculture/CommonCultureDetailCategoryManagerTest.java
@@ -1,5 +1,7 @@
 package com.plzgraduate.myongjigraduatebe.graduation.domain.service.commonculture;
 
+import static com.plzgraduate.myongjigraduatebe.fixture.CommonCultureFixture.*;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashSet;
@@ -7,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
@@ -72,6 +75,50 @@ class CommonCultureDetailCategoryManagerTest {
 		assertThat(detailCategoryResult)
 			.extracting("detailCategoryName", "isCompleted", "totalCredits")
 			.contains(commonCultureCategoryName, false, categoryTotalCredit);
+	}
+
+	@DisplayName("16~19 학번의 기독교 카테고리는 필수 과목을 수강해야 이수 완료의 카테고리 졸업 결과를 생성할 수 있다.")
+	@Test
+	void generateMandatorySatisfactionCommonCultureDetailCategory() {
+		//given
+		User user = UserFixture.경영학과_19학번();
+		Set<TakenLecture> takenLectures = new HashSet<>((Set.of(
+			TakenLecture.of(user, mockLectureMap.get("KMA00101"), 2019, Semester.FIRST),
+			TakenLecture.of(user, mockLectureMap.get("KMA02102"), 2019, Semester.FIRST),
+			TakenLecture.of(user, mockLectureMap.get("KMA02122"), 2019, Semester.FIRST),
+			TakenLecture.of(user, mockLectureMap.get("KMA02104"), 2023, Semester.FIRST),
+			TakenLecture.of(user, mockLectureMap.get("KMA02141"), 2023, Semester.FIRST)
+		)));
+		Set<CommonCulture> graduationLectures = 공통교양_16_17(); // == 공통교양_18_19
+
+		//when
+		DetailCategoryResult detailCategoryResult = manager.generate(takenLectures, graduationLectures, CHRISTIAN_A);
+
+		//then
+		assertThat(detailCategoryResult)
+			.extracting("detailCategoryName", "isCompleted", "isSatisfiedMandatory", "totalCredits")
+			.contains(CHRISTIAN_A.getName(), true, true, CHRISTIAN_A.getTotalCredit());
+	}
+
+	@DisplayName("16~19 학번의 기독교 카테고리는 필수 과목을 수강하지 않으면 수강 학점이 카테고리의 총 학점 이상이어도 이수 미완료 졸업 결과를 생성한다.")
+	@Test
+	void generateMandatoryUnSatisfactionCommonCultureDetailCategory() {
+		//given
+		User user = UserFixture.경영학과_19학번();
+		Set<TakenLecture> takenLectures = new HashSet<>((Set.of(
+			TakenLecture.of(user, mockLectureMap.get("KMA02102"), 2019, Semester.FIRST),
+			TakenLecture.of(user, mockLectureMap.get("KMA02122"), 2023, Semester.FIRST),
+			TakenLecture.of(user, mockLectureMap.get("KMA02103"), 2023, Semester.FIRST)
+		)));
+		Set<CommonCulture> graduationLectures = 공통교양_16_17(); // == 공통교양_18_19
+
+		//when
+		DetailCategoryResult detailCategoryResult = manager.generate(takenLectures, graduationLectures, CHRISTIAN_A);
+
+		//then
+		assertThat(detailCategoryResult)
+			.extracting("detailCategoryName", "isCompleted", "isSatisfiedMandatory", "totalCredits")
+			.contains(CHRISTIAN_A.getName(), false, false, CHRISTIAN_A.getTotalCredit());
 	}
 
 }
