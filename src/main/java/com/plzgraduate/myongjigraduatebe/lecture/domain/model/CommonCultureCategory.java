dbf267f14ab856418a0e7f3c72839efb6cb6e812
5uhwann <5uhwann27@gmail.com>
2023-08-10 17:07:21 +0900

    refactor: 공통교양 - 영어 카테고리 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/domain/model/CommonCultureCategory.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/domain/model/CommonCultureCategory.java
@@ -11,6 +11,7 @@ public enum CommonCultureCategory {
 	CHRISTIAN_A("기독교", 4, List.of(16, 17, 18, 19)),
 	CHRISTIAN_B("기독교",4, List.of(20, 21, 22, 23)),
 	EXPRESSION("사고와 표현", 3, List.of(16, 17, 18, 19, 20 ,21, 22, 23)),
+	ENGLISH("영어", 6, List.of(116, 17, 18, 19, 20 ,21, 22, 23)),
 	CAREER("진로", 2, List.of(18, 19, 20, 21, 22)),
 	DIGITAL_LITERACY("진로와디지털리터러시", 2,  List.of(23));
 
