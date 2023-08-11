f213b23fc21e65c1c92da00807b99bfd13a79c15
5uhwann <5uhwann27@gmail.com>
2023-08-11 20:05:50 +0900

    test: 핵심교양 fixture 작성

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/fixture/CoreCultureFixture.java
@@ -0,0 +1,82 @@
+package com.plzgraduate.myongjigraduatebe.fixture;
+
+import static com.plzgraduate.myongjigraduatebe.fixture.UserFixture.경영학과_23학번;
+import static com.plzgraduate.myongjigraduatebe.fixture.UserFixture.영문학과_16학번;
+import static com.plzgraduate.myongjigraduatebe.fixture.UserFixture.영문학과_18학번;
+import static com.plzgraduate.myongjigraduatebe.fixture.UserFixture.행정학과_21학번;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.CAREER;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.CHRISTIAN_A;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.CHRISTIAN_B;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.ENGLISH;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.EXPRESSION;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CoreCultureCategory.*;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.CoreCulture;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.Lecture;
+
+public class CoreCultureFixture implements ArgumentsProvider {
+
+	public static final Map<String, Lecture> mockLectureMap = LectureFixture.getMockLectureMap();
+
+	@Override
+	public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+		return Stream.of(
+			Arguments.arguments(HISTORY_PHILOSOPHY,핵심교양_역사와철학()),
+			Arguments.arguments(SOCIETY_COMMUNITY, 핵심교양_사회와공동체()),
+			Arguments.arguments(CULTURE_ART, 핵심교양_문화와예술()),
+			Arguments.arguments(SCIENCE_TECHNOLOGY, 핵심교양_과학과기술())
+		);
+	}
+
+	public static Set<CoreCulture> 핵심교양_역사와철학() {
+		Set<CoreCulture> lectureSet = new HashSet<>();
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02110"), HISTORY_PHILOSOPHY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02111"), HISTORY_PHILOSOPHY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02112"), HISTORY_PHILOSOPHY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02140"), HISTORY_PHILOSOPHY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02158"), HISTORY_PHILOSOPHY));
+		return lectureSet;
+	}
+
+	public static Set<CoreCulture> 핵심교양_사회와공동체() {
+		Set<CoreCulture> lectureSet = new HashSet<>();
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02113"), SOCIETY_COMMUNITY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02114"), SOCIETY_COMMUNITY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02131"), SOCIETY_COMMUNITY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02142"), SOCIETY_COMMUNITY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02160"), SOCIETY_COMMUNITY));
+		return lectureSet;
+	}
+
+	public static Set<CoreCulture> 핵심교양_문화와예술() {
+		Set<CoreCulture> lectureSet = new HashSet<>();
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02128"), CULTURE_ART));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02130"), CULTURE_ART));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02132"), CULTURE_ART));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02155"), CULTURE_ART));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02156"), CULTURE_ART));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02159"), CULTURE_ART));
+		return lectureSet;
+	}
+
+	public static Set<CoreCulture> 핵심교양_과학과기술() {
+		Set<CoreCulture> lectureSet = new HashSet<>();
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02119"), SCIENCE_TECHNOLOGY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02120"), SCIENCE_TECHNOLOGY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02127"), SCIENCE_TECHNOLOGY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02135"), SCIENCE_TECHNOLOGY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02136"), SCIENCE_TECHNOLOGY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02138"), SCIENCE_TECHNOLOGY));
+		lectureSet.add(CoreCulture.of(mockLectureMap.get("KMA02139"), SCIENCE_TECHNOLOGY));
+		return lectureSet;
+	}
+}
