a5aeff1984da3eb47e4dc7327ad14d9b3d6b0a7b
stophwan <jjw981210@gmail.com>
2023-08-15 21:49:14 +0900

    feat: Major 도메인 필드 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/domain/model/Major.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/domain/model/Major.java
@@ -8,15 +8,37 @@ public class Major {
 
 	private final Lecture lecture;
 	private final String department;
-	private final int isMandatory;
-	private final int startYear;
+	private int isMandatory;
+	private final int appliedStartEntryYear;
+	private final int appliedEndEntryYear;
 
 	@Builder
-	private Major(Lecture lecture, String department, int isMandatory, int startYear) {
+	private Major(Lecture lecture, String department, int isMandatory, int appliedStartEntryYear, int appliedEndEntryYear) {
 		this.lecture = lecture;
 		this.department = department;
 		this.isMandatory = isMandatory;
-		this.startYear = startYear;
+		this.appliedStartEntryYear = appliedStartEntryYear;
+		this.appliedEndEntryYear = appliedEndEntryYear;
+	}
+
+	public static Major of(Lecture lecture, String department, int isMandatory, int appliedStartEntryYear, int appliedEndEntryYear) {
+		return Major.builder()
+			.lecture(lecture)
+			.department(department)
+			.isMandatory(isMandatory)
+			.appliedStartEntryYear(appliedStartEntryYear)
+			.appliedEndEntryYear(appliedEndEntryYear)
+			.build();
+	}
+
+	public void changeMandatoryToElectiveByEntryYearRange(int entryYear) {
+		if(checkMandatoryByEntryYear(entryYear)) {
+			isMandatory = 0;
+		}
+	}
+
+	private boolean checkMandatoryByEntryYear(int entryYear) {
+		return isMandatory==1 && !(entryYear >= appliedStartEntryYear && entryYear <= appliedEndEntryYear);
 	}
 
 }
