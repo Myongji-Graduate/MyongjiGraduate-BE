02571289292dc417368c852145270b5e87b68611
stophwan <jjw981210@gmail.com>
2023-08-15 21:52:43 +0900

    feat: 전공 졸업사정결과 계산 기능 구현

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/major/ElectiveMajorManager.java
@@ -0,0 +1,27 @@
+package com.plzgraduate.myongjigraduatebe.graduation.domain.service.major;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailCategoryResult;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.Lecture;
+import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLecture;
+
+public class ElectiveMajorManager {
+	public DetailCategoryResult createDetailCategoryResult(Set<TakenLecture> takenLectures,
+		Set<Lecture> electiveLectures, int electiveMajorTotalCredit) {
+		Set<Lecture> takenElective = new HashSet<>();
+		Set<TakenLecture> finishedTakenLecture = new HashSet<>();
+		takenLectures.stream()
+			.filter(takenLecture -> electiveLectures.contains(takenLecture.getLecture()))
+			.forEach(takenLecture -> {
+				finishedTakenLecture.add(takenLecture);
+				takenElective.add(takenLecture.getLecture());
+			});
+		DetailCategoryResult electiveMajorResult = DetailCategoryResult.create("전공선택", true, electiveMajorTotalCredit);
+		electiveMajorResult.calculate(takenElective, electiveLectures);
+		takenLectures.removeAll(finishedTakenLecture);
+
+		return electiveMajorResult;
+	}
+}
---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/major/MajorManager.java
@@ -0,0 +1,86 @@
+package com.plzgraduate.myongjigraduatebe.graduation.domain.service.major;
+
+import static com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory.MAJOR;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailCategoryResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.GraduationManager;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.major.exception.MajorExceptionHandler;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.major.exception.OptionalMandatoryHandler;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.major.exception.ReplaceMandatoryMajorHandler;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.Lecture;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.Major;
+import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLecture;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.StudentInformation;
+
+public class MajorManager implements GraduationManager<Major> {
+
+	@Override
+	public DetailGraduationResult createDetailGraduationResult(StudentInformation studentInformation,
+		Set<TakenLecture> takenLectures, Set<Major> majorLectures, int graduationResultTotalCredit) {
+
+		removeDuplicateLecture(takenLectures, majorLectures);
+		changeMandatoryToElectiveByMajorRange(studentInformation, majorLectures);
+
+		Set<Lecture> mandatoryLectures = filterMandatoryLectures(majorLectures);
+		Set<Lecture> electiveLectures = filterElectiveLectures(majorLectures);
+
+		List<MajorExceptionHandler> majorExceptionHandlers = List.of(new OptionalMandatoryHandler(), new ReplaceMandatoryMajorHandler());
+		MandatoryMajorManager mandatoryMajorManager = new MandatoryMajorManager(majorExceptionHandlers);
+		ElectiveMajorManager electiveMajorManager = new ElectiveMajorManager();
+
+		DetailCategoryResult mandantoryDetailCategoryResult = mandatoryMajorManager.createDetailCategoryResult(
+			studentInformation, takenLectures,
+			mandatoryLectures, electiveLectures);
+
+		int electiveMajorTotalCredit = graduationResultTotalCredit - mandantoryDetailCategoryResult.getTotalCredits();
+		DetailCategoryResult electiveDetailCategoryResult = electiveMajorManager.createDetailCategoryResult(
+			takenLectures, electiveLectures, electiveMajorTotalCredit);
+
+		return DetailGraduationResult.create(MAJOR, graduationResultTotalCredit,
+			List.of(mandantoryDetailCategoryResult, electiveDetailCategoryResult));
+	}
+
+	private Set<Lecture> filterMandatoryLectures(Set<Major> majorLectures) {
+		return majorLectures.stream()
+			.filter(major -> major.getIsMandatory() == 1)
+			.map(Major::getLecture)
+			.collect(Collectors.toSet());
+	}
+
+	private Set<Lecture> filterElectiveLectures(Set<Major> majorLectures) {
+		return majorLectures.stream()
+			.filter(major -> major.getIsMandatory() == 0)
+			.map(Major::getLecture)
+			.collect(Collectors.toSet());
+	}
+
+	private void removeDuplicateLecture(Set<TakenLecture> takenLectures, Set<Major> graduationLectures) {
+		Set<Lecture> duplicatedTakenLectures = findDuplicatedTakenLecture(takenLectures);
+		graduationLectures.removeIf(graduationLecture ->
+			duplicatedTakenLectures.stream()
+				.anyMatch(duplicatedTakenLecture ->
+					!duplicatedTakenLecture.equals(graduationLecture.getLecture())
+						&& duplicatedTakenLecture.getDuplicateCode().equals(graduationLecture.getLecture().getDuplicateCode())
+				)
+		);
+	}
+
+	private Set<Lecture> findDuplicatedTakenLecture(Set<TakenLecture> takenLectures) {
+		return takenLectures.stream()
+			.map(TakenLecture::getLecture)
+			.filter(lecture -> lecture.getDuplicateCode() != null && lecture.getIsRevoked() == 1)
+			.collect(Collectors.toSet());
+	}
+
+	private void changeMandatoryToElectiveByMajorRange(StudentInformation studentInformation,
+		Set<Major> majorsLectures) {
+		majorsLectures.forEach(major ->
+			major.changeMandatoryToElectiveByEntryYearRange(studentInformation.getEntryYear()));
+	}
+
+}
