4a76e18c7f74f9475e839930b3452105f199feca
5uhwann <5uhwann27@gmail.com>
2023-08-17 15:57:16 +0900

    test: Lecture 교양 과목 여부 확인 테스트 작성

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/lecture/domain/model/LectureTest.java
@@ -0,0 +1,38 @@
+package com.plzgraduate.myongjigraduatebe.lecture.domain.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LectureTest {
+
+	@DisplayName("과목코드가 'KM'으로 시작하는 과목은 교양과목이다.")
+	@Test
+	void checkIsCulture() {
+	    //given
+		String lectureCode = "KMA02100";
+		Lecture lecture = Lecture.from(lectureCode);
+
+		//when
+		boolean isCulture = lecture.isCulture();
+
+		//then
+		assertThat(isCulture).isTrue();
+	}
+
+	@DisplayName("과목코드가 'KM'으로 시작하지 않는 과목은 교양과목이 아니다.")
+	@Test
+	void checkNotIsCulture() {
+		//given
+		String lectureCode = "HEB01102";
+		Lecture lecture = Lecture.from(lectureCode);
+
+		//when
+		boolean isCulture = lecture.isCulture();
+
+		//then
+		assertThat(isCulture).isFalse();
+	}
+
+}
