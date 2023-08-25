39ea0bfc143ecf26c9b00c842dc25b43bc13c282
stophwan <jjw981210@gmail.com>
2023-08-25 19:04:48 +0900

    test: persistence adapter 테스트코드 작성

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/user/adaptor/out/persistence/UserPersistenceAdaptorTest.java
@@ -0,0 +1,69 @@
+package com.plzgraduate.myongjigraduatebe.user.adaptor.out.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.plzgraduate.myongjigraduatebe.support.PersistenceTestSupport;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
+
+class UserPersistenceAdaptorTest extends PersistenceTestSupport {
+
+	@Autowired
+	private UserPersistenceAdaptor userPersistenceAdaptor;
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	void tearDown() {
+		userRepository.deleteAllInBatch();
+	}
+
+	@DisplayName("사용자를 저장한다.")
+	@Test
+	void 사용자_저장() {
+	    //given
+		User user = createUser("mju1001", "1q2w3e4r!", "60181666");
+	    //when
+		userPersistenceAdaptor.saveUser(user);
+	    //then
+		assertThat(userRepository.findByAuthId("mju1001").isPresent()).isTrue();
+	}
+
+	@DisplayName("아이디로 사용자를 조회한다.")
+	@Test
+	void 아아디_사용자_조회() {
+		//given
+		String authId = "mju1001";
+		UserJpaEntity userEntity = createUserEntity(authId, "1q2w3e4r!", "60181666");
+		userRepository.save(userEntity);
+		//when
+		User user = userPersistenceAdaptor.loadUserByAuthId(authId);
+
+		//then
+		assertThat(user.getAuthId()).isEqualTo(authId);
+
+	}
+
+	private User createUser(String authId, String password, String studentNumber) {
+		return User
+			.builder()
+			.authId(authId)
+			.password(password)
+			.studentNumber(studentNumber)
+			.build();
+	}
+
+	private UserJpaEntity createUserEntity(String authId, String password, String studentNumber) {
+		return UserJpaEntity
+			.builder()
+			.authId(authId)
+			.password(password)
+			.studentNumber(studentNumber)
+			.build();
+	}
+}
---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/user/adaptor/out/persistence/UserRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.plzgraduate.myongjigraduatebe.user.adaptor.out.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.plzgraduate.myongjigraduatebe.support.PersistenceTestSupport;
+
+class UserRepositoryTest extends PersistenceTestSupport {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@DisplayName("아이디로 사용자를 조회한다.")
+	@Test
+	void 아이디_사용자_조회() {
+		//given
+		UserJpaEntity user1 = createUserEntity("mju1001", "1q2w3e4r!", "60181666");
+		UserJpaEntity user2 = createUserEntity("mju1002", "1q2w3e4r!", "60181667");
+		userRepository.saveAll(List.of(user1, user2));
+		//when
+		Optional<UserJpaEntity> user = userRepository.findByAuthId("mju1001");
+
+		//then
+		assertThat(user.isPresent()).isTrue();
+		assertThat(user.get().getAuthId()).isEqualTo("mju1001");
+	}
+
+	private UserJpaEntity createUserEntity(String authId, String password, String studentNumber) {
+		return UserJpaEntity
+			.builder()
+			.authId(authId)
+			.password(password)
+			.studentNumber(studentNumber)
+			.build();
+	}
+
+}
