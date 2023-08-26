dec4819252c68af9371c1ea7a6f6da8a44b22056
stophwan <jjw981210@gmail.com>
2023-08-26 12:54:59 +0900

    feat: 회원가입, 로그인 입력 검증 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/auth/adaptor/in/web/request/SignInRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/auth/adaptor/in/web/request/SignInRequest.java
@@ -1,5 +1,9 @@
 package com.plzgraduate.myongjigraduatebe.auth.adaptor.in.web.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import com.plzgraduate.myongjigraduatebe.auth.application.port.command.SignInCommand;
 
 import lombok.Getter;
@@ -9,8 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignInRequest {
 
+	@NotBlank(message = "아아디를 입력해주세요.")
+	@Size(min = 6, max = 20, message = "아이디는 8자에서 20자 사이여야합니다.")
 	private String authId;
 
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$", message = "비밀번호는 문자, 숫자, 기호가 1개 이상 포함되어야합니다.")
 	private String password;
 
 	public SignInCommand toCommand() {
@@ -20,6 +29,4 @@ public class SignInRequest {
 			.password(password)
 			.build();
 	}
-
-
 }
--- src/main/java/com/plzgraduate/myongjigraduatebe/user/adaptor/in/web/request/SignUpRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/adaptor/in/web/request/SignUpRequest.java
@@ -1,5 +1,9 @@
 package com.plzgraduate.myongjigraduatebe.user.adaptor.in.web.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import com.plzgraduate.myongjigraduatebe.user.application.port.in.command.SignUpCommand;
 import com.plzgraduate.myongjigraduatebe.user.domain.model.EnglishLevel;
 
@@ -9,12 +13,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SignUpRequest {
+
+	@NotBlank(message = "아아디를 입력해주세요.")
+	@Size(min = 6, max = 20, message = "아이디는 8자에서 20자 사이여야합니다.")
 	private String authId;
 
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$", message = "비밀번호는 문자, 숫자, 기호가 1개 이상 포함되어야합니다.")
 	private String password;
 
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	@Pattern(regexp = "\\d{8}", message = "학번은 8자리 숫자여야 합니다.")
 	private String studentNumber;
 
+	@NotBlank(message = "영어레벨을 입력해주세요.")
 	private String engLv;
 
 	public SignUpCommand toCommand() {
