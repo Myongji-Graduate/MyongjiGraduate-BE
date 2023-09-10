fd8bcdc718d56a6f4d924be5f6b477be4932ede0
5uhwann <5uhwann27@gmail.com>
2023-09-11 02:37:54 +0900

    test: CheckGraduationService 테스트 작성

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/graduation/application/port/CheckGraduationServiceTest.java
@@ -0,0 +1,124 @@
+package com.plzgraduate.myongjigraduatebe.graduation.application.port;
+
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCultureCategory.*;
+import static com.plzgraduate.myongjigraduatebe.lecture.domain.model.CoreCultureCategory.*;
+import static com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.Semester.*;
+import static com.plzgraduate.myongjigraduatebe.user.domain.model.College.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.plzgraduate.myongjigraduatebe.fixture.LectureFixture;
+import com.plzgraduate.myongjigraduatebe.fixture.UserFixture;
+import com.plzgraduate.myongjigraduatebe.graduation.adpater.in.web.response.GraduationResponse;
+import com.plzgraduate.myongjigraduatebe.graduation.application.port.out.LoadGraduationRequirementPort;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationRequirement;
+import com.plzgraduate.myongjigraduatebe.lecture.application.port.out.LoadBasicAcademicalCulturePort;
+import com.plzgraduate.myongjigraduatebe.lecture.application.port.out.LoadCommonCulturePort;
+import com.plzgraduate.myongjigraduatebe.lecture.application.port.out.LoadCoreCulturePort;
+import com.plzgraduate.myongjigraduatebe.lecture.application.port.out.LoadMajorPort;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.BasicAcademicalCulture;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCulture;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.CoreCulture;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.Lecture;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.Major;
+import com.plzgraduate.myongjigraduatebe.takenlecture.application.port.out.LoadTakenLecturePort;
+import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLecture;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
+
+@ExtendWith(MockitoExtension.class)
+class CheckGraduationServiceTest {
+
+	Map<String, Lecture> mockLectureMap = LectureFixture.getMockLectureMap();
+
+	@Mock
+	private LoadGraduationRequirementPort loadGraduationRequirementPort;
+	@Mock
+	private LoadTakenLecturePort loadTakenLecturePort;
+	@Mock
+	private LoadCommonCulturePort loadCommonCulturePort;
+	@Mock
+	private LoadCoreCulturePort loadCoreCulturePort;
+	@Mock
+	private LoadBasicAcademicalCulturePort loadBasicAcademicalCulturePort;
+	@Mock
+	private LoadMajorPort loadMajorPort;
+	@InjectMocks
+	private CheckGraduationService checkGraduationService;
+
+	@DisplayName("유저 정보, 유저의 수강 과목 정보로 졸업 결과를 확인한다.")
+	@Test
+	void checkGraduation() {
+		//given
+		User user = UserFixture.응용소프트웨어학과_19학번();
+		given(loadGraduationRequirementPort.loadGraduationRequirement(user))
+			.willReturn(GraduationRequirement.builder()
+				.commonCultureCredit(15)
+				.coreCultureCredit(12)
+				.basicAcademicalCredit(3)
+				.majorCredit(3)
+				.normalCultureCredit(3)
+				.freeElectiveCredit(3)
+				.totalCredit(41).build());
+		given(loadTakenLecturePort.loadTakenLectures(user))
+			.willReturn(new HashSet<>(Set.of(
+				TakenLecture.of(user, mockLectureMap.get("KMA00101"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02102"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02104"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02106"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02107"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02108"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02109"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02137"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02112"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02114"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02128"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02135"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMB02123"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("HED01206"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02101"), 2019, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02101"), 2019, SECOND),
+				TakenLecture.of(user, mockLectureMap.get("KMA02101"), 2020, FIRST),
+				TakenLecture.of(user, mockLectureMap.get("KMA02101"), 2020, SECOND),
+				TakenLecture.of(user, mockLectureMap.get("KMA02113"), 2020, SECOND),
+				TakenLecture.of(user, mockLectureMap.get("KMA02130"), 2020, SECOND)
+			)));
+		given(loadCommonCulturePort.loadCommonCulture(user))
+			.willReturn(new HashSet<>(Set.of(
+				CommonCulture.of(mockLectureMap.get("KMA00101"), CHRISTIAN_A),
+				CommonCulture.of(mockLectureMap.get("KMA02102"), CHRISTIAN_A),
+				CommonCulture.of(mockLectureMap.get("KMA02104"), EXPRESSION),
+				CommonCulture.of(mockLectureMap.get("KMA02106"), ENGLISH),
+				CommonCulture.of(mockLectureMap.get("KMA02107"), ENGLISH),
+				CommonCulture.of(mockLectureMap.get("KMA02108"), ENGLISH),
+				CommonCulture.of(mockLectureMap.get("KMA02109"), ENGLISH),
+				CommonCulture.of(mockLectureMap.get("KMA02137"), CAREER))));
+		given(loadCoreCulturePort.loadCoreCulture(user))
+			.willReturn(new HashSet<>(Set.of(
+				CoreCulture.of(mockLectureMap.get("KMA02112"), HISTORY_PHILOSOPHY),
+				CoreCulture.of(mockLectureMap.get("KMA02114"), SOCIETY_COMMUNITY),
+				CoreCulture.of(mockLectureMap.get("KMA02128"), CULTURE_ART),
+				CoreCulture.of(mockLectureMap.get("KMA02135"), SCIENCE_TECHNOLOGY))));
+		given(loadBasicAcademicalCulturePort.loadBasicAcademicalCulture(user))
+			.willReturn(new HashSet<>(
+				Set.of(BasicAcademicalCulture.of(mockLectureMap.get("KMB02123"), ICT.getText()))));
+		given((loadMajorPort.loadMajor(user)))
+			.willReturn(new HashSet<>(Set.of(Major.of(mockLectureMap.get("HED01206"), "응용소프트웨어", 1, 18, 20))));
+
+		//when
+		GraduationResponse graduationResponse = checkGraduationService.checkGraduation(user);
+
+		//then
+		assertThat(graduationResponse.isGraduated()).isTrue();
+	}
+}
