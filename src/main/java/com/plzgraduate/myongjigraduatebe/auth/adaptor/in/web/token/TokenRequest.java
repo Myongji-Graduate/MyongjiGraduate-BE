65fbd827b6c4e44d6709f3e07d1c45b885ac26c5
stophwan <jjw981210@gmail.com>
2023-09-24 22:54:17 +0900

    feat: controller validatation 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/auth/adaptor/in/web/token/TokenRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/auth/adaptor/in/web/token/TokenRequest.java
@@ -1,5 +1,7 @@
 package com.plzgraduate.myongjigraduatebe.auth.adaptor.in.web.token;
 
+import javax.validation.constraints.NotBlank;
+
 import com.plzgraduate.myongjigraduatebe.auth.application.port.in.token.TokenCommand;
 
 import lombok.Builder;
@@ -9,6 +11,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class TokenRequest {
+
+	@NotBlank(message = "unexpected token")
 	private String refreshToken;
 
 	@Builder
