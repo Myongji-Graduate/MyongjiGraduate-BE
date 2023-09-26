c6a5451335219fac7ddcbddcba476c8de5bb4859
5uhwann <5uhwann27@gmail.com>
2023-09-26 23:17:46 +0900

    feat: 회원 탈퇴 컨트롤러 연결

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/user/adaptor/in/web/withdraw/WithDrawControllerTest.java
@@ -0,0 +1,43 @@
+package com.plzgraduate.myongjigraduatebe.user.adaptor.in.web.withdraw;
+
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.plzgraduate.myongjigraduatebe.support.WebAdaptorTestSupport;
+import com.plzgraduate.myongjigraduatebe.support.WithMockAuthenticationUser;
+import com.plzgraduate.myongjigraduatebe.user.application.port.in.withdraw.WithDrawUserUseCase;
+
+@WebMvcTest(controllers = WithDrawController.class)
+class WithDrawControllerTest extends WebAdaptorTestSupport {
+
+	@MockBean
+	private WithDrawUserUseCase withDrawUserUseCase;
+
+	@WithMockAuthenticationUser
+	@DisplayName("유저의 회원 탈퇴 요청을 수행한다.")
+	@Test
+	void withDraw() throws Exception {
+	    //given
+		Long userId = 1L;
+	
+	    //when
+		ResultActions actions = mockMvc.perform(
+			delete("/api/v1/users")
+				.with(csrf()));
+	
+	    //then
+		then(withDrawUserUseCase).should().withDraw(userId);
+		actions.
+			andDo(print())
+			.andExpect(status().isOk());
+	}
+}
