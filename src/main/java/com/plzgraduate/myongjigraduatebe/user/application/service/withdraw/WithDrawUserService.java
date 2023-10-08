e30fb5a58ce78ffc54d985725a6f5dc6cc224d40
5uhwann <5uhwann27@gmail.com>
2023-10-09 01:48:17 +0900

    refactor: 회원 탈퇴 시 유저 파싱 정보 삭제 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/application/service/withdraw/WithDrawUserService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/application/service/withdraw/WithDrawUserService.java
@@ -3,6 +3,7 @@ package com.plzgraduate.myongjigraduatebe.user.application.service.withdraw;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.plzgraduate.myongjigraduatebe.core.meta.UseCase;
+import com.plzgraduate.myongjigraduatebe.parsing.application.port.out.DeleteParsingTextHistoryPort;
 import com.plzgraduate.myongjigraduatebe.takenlecture.application.port.in.delete.DeleteTakenLectureByUserUseCase;
 import com.plzgraduate.myongjigraduatebe.user.application.port.in.find.FindUserUseCase;
 import com.plzgraduate.myongjigraduatebe.user.application.port.in.withdraw.WithDrawUserUseCase;
@@ -18,12 +19,14 @@ class WithDrawUserService implements WithDrawUserUseCase {
 
 	private final FindUserUseCase findUserUseCase;
 	private final DeleteTakenLectureByUserUseCase deleteTakenLectureByUserUseCase;
+	private final DeleteParsingTextHistoryPort deleteParsingTextHistoryPort;
 	private final DeleteUserPort deleteUserPort;
 
 	@Override
 	public void withDraw(Long userId) {
 		User user = findUserUseCase.findUserById(userId);
 		deleteTakenLectureByUserUseCase.deleteAllTakenLecturesByUser(user);
+		deleteParsingTextHistoryPort.deleteUserParsingTextHistory(user);
 		deleteUserPort.deleteUser(user);
 	}
 }
