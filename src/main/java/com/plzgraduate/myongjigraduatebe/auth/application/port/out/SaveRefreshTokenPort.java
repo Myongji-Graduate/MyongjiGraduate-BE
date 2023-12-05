249be0c32ab0e11f155459258f1f57d8d98e6596
stophwan <jjw981210@gmail.com>
2023-12-06 06:36:33 +0900

    feat: set time based InMemoryRepository

--- src/main/java/com/plzgraduate/myongjigraduatebe/auth/application/port/out/SaveRefreshTokenPort.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/auth/application/port/out/SaveRefreshTokenPort.java
@@ -1,7 +1,6 @@
 package com.plzgraduate.myongjigraduatebe.auth.application.port.out;
 
-import com.plzgraduate.myongjigraduatebe.auth.domain.RefreshToken;
 
 public interface SaveRefreshTokenPort {
-	void saveRefreshToken(RefreshToken refreshToken);
+	void saveRefreshToken(String refreshToken, Long userId);
 }
