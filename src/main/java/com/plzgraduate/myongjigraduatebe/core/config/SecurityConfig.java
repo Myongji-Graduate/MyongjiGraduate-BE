ec4ad473ec56415a42c11b13edc1f10528a255d1
stophwan <jjw981210@gmail.com>
2023-12-06 17:36:25 +0900

    fix: cors 수정

--- src/main/java/com/plzgraduate/myongjigraduatebe/core/config/SecurityConfig.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/core/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
 		return http.build();
 	}
 
+	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
