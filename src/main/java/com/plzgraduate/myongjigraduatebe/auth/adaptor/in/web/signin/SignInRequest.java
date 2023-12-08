4b94d0a4b6d40d8126bfa56ad68b162920003daa
stophwan <jjw981210@gmail.com>
2023-12-08 22:38:08 +0900

    refactor: 로그인시 아이디 비밀번호 형식 오류를 400 -> 401로 통일

--- src/main/java/com/plzgraduate/myongjigraduatebe/auth/adaptor/in/web/signin/SignInRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/auth/adaptor/in/web/signin/SignInRequest.java
@@ -1,8 +1,6 @@
 package com.plzgraduate.myongjigraduatebe.auth.adaptor.in.web.signin;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import com.plzgraduate.myongjigraduatebe.auth.application.port.in.signin.SignInCommand;
 
@@ -15,11 +13,9 @@ import lombok.NoArgsConstructor;
 public class SignInRequest {
 
 	@NotBlank(message = "아아디를 입력해주세요.")
-	@Size(min = 6, max = 20, message = "아이디는 6자에서 20자 사이여야합니다.")
 	private String authId;
 
 	@NotBlank(message = "비밀번호를 입력해주세요.")
-	@Pattern(regexp = "^(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9]).{8,20}$", message = "비밀번호는 특수문자를 포함한 8자에서 20자 사이여야합니다.")
 	private String password;
 
 	@Builder
