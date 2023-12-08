9004aeaaca6f9f9dd6d598b98a41f344ee9d21dc
정지환 <64758861+stophwan@users.noreply.github.com>
2023-12-08 23:27:21 +0900

    refactor: 규칙 수정에 따른 비밀번호 재설정 API 변경 (#213)
    * refactor: 규칙 수정에 따른 비밀번호 재설정 변경
    
    * refactor: fix code smell

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/adaptor/in/web/resetpassword/ResetPasswordRequest.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/adaptor/in/web/resetpassword/ResetPasswordRequest.java
@@ -2,7 +2,6 @@ package com.plzgraduate.myongjigraduatebe.user.adaptor.in.web.resetpassword;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import com.plzgraduate.myongjigraduatebe.user.application.port.in.resetpassword.ResetPasswordCommand;
 
@@ -18,13 +17,11 @@ public class ResetPasswordRequest {
 	private String authId;
 
 	@NotBlank(message = "비밀번호를 입력해주세요.")
-	@Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야합니다.")
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$", message = "비밀번호는 문자, 숫자, 기호가 1개 이상 포함되어야합니다.")
+	@Pattern(regexp = "^(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9]).{8,20}$", message = "비밀번호는 특수문자를 포함한 8자에서 20자 사이여야합니다.")
 	private String newPassword;
 
 	@NotBlank(message = "비밀번호 확인을 입력해주세요.")
-	@Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야합니다.")
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$", message = "비밀번호는 문자, 숫자, 기호가 1개 이상 포함되어야합니다.")
+	@Pattern(regexp = "^(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9]).{8,20}$", message = "비밀번호는 특수문자를 포함한 8자에서 20자 사이여야합니다.")
 	private String passwordCheck;
 
 	@Builder
