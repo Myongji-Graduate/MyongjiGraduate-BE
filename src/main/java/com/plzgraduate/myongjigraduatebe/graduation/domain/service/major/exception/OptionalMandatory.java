7e2d2820b3d5b0e2522f9e111656155109fb78ae
정지환 <64758861+stophwan@users.noreply.github.com>
2023-12-16 00:19:57 +0900

    fix: 행정학과, 경영정보학과 전공선택필수 과목 추가 (#219)
    * fix: 행정학과, 경영정보학과 전공선택필수 과목 추가
    
    * style: 중복과목 제거에 대한 주석 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/major/exception/OptionalMandatory.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/major/exception/OptionalMandatory.java
@@ -13,7 +13,14 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OptionalMandatory {
-	ADMINISTRATIONS("행정학과", 2, Set.of()),
+	ADMINISTRATIONS("행정학과", 2, Set.of(
+		Lecture.of("HBA01109", "행정학의이해", 3, 0, null),
+		Lecture.of("HBA01110", "리더십과조직행동", 3, 0, null),
+		Lecture.of("HBA01111", "감성과현대행정", 3, 0, null),
+		Lecture.of("HBA01112", "비교정책론", 3, 0, null),
+		Lecture.of("HBA01113", "비영리조직", 3, 0, null),
+		Lecture.of("HBA01222", "커뮤니티개발연구", 3, 0, null)
+	)),
 	BUSINESS("경영학과", 4, Set.of(
 		Lecture.of("HBX01104", "회계원리", 3, 0, null),
 		Lecture.of("HBX01113", "인적자원관리", 3, 0, null),
@@ -30,10 +37,14 @@ public enum OptionalMandatory {
 		Lecture.of("HBX01114", "생산운영관리", 3, 1, null),
 		Lecture.of("HBX01143", "운영관리", 3, 0, null)
 	)),
-	MANAGEMENT_INFORMATION("경영정보학과", 3, Set.of());
+	MANAGEMENT_INFORMATION("경영정보학과", 2, Set.of(
+		Lecture.of("HBX01113", "인적자원관리", 3, 0, null),
+		Lecture.of("HBX01106", "마케팅원론", 3, 0, null),
+		Lecture.of("HBX01105", "재무관리원론",3, 0, null)
+	));
 
 	private final String department;
-	private final int chooseNUmber;
+	private final int chooseNumber;
 	private final Set<Lecture> optionalMandatoryLectures;
 
 	public static OptionalMandatory from(User user) {
@@ -52,7 +63,7 @@ public enum OptionalMandatory {
 	}
 
 	public int getChooseLectureCredit(OptionalMandatory optionalMandatory) {
-		return optionalMandatory.chooseNUmber * 3;
+		return optionalMandatory.chooseNumber * 3;
 	}
 
 }
--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/major/exception/OptionalMandatoryHandler.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/domain/service/major/exception/OptionalMandatoryHandler.java
@@ -12,13 +12,23 @@ import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
 import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLectureInventory;
 
 public class OptionalMandatoryHandler implements MajorExceptionHandler {
+
+	private static final String MANAGEMENT_INFORMATION = "경영정보학과";
+	private static final String BUSINESS = "경영학과";
+	private static final String ADMINISTRATIONS = "행정학과";
+	private static final String INTERNATIONAL_TRADE = "국제통상학과";
+	private static final int CLASS_OF_17 = 17;
+	private static final int CLASS_OF_19 = 19;
 	private int removedMandatoryTotalCredit = 0;
 
 	public boolean isSupport(User user) {
-		if (user.getMajor().equals("경영정보학과") && user.getEntryYear() >= 19) {
+		if (user.getMajor().equals(MANAGEMENT_INFORMATION) && user.getEntryYear() >= CLASS_OF_19) {
 			return true;
 		}
-		return List.of("행정학과", "경영학과", "국제통상학과").contains(user.getMajor());
+		if (user.getMajor().equals(ADMINISTRATIONS) && user.getEntryYear() >= CLASS_OF_17) {
+			return true;
+		}
+		return List.of(BUSINESS, INTERNATIONAL_TRADE).contains(user.getMajor());
 	}
 
 	@Override
@@ -46,9 +56,11 @@ public class OptionalMandatoryHandler implements MajorExceptionHandler {
 		TakenLectureInventory takenLectureInventory, Set<Lecture> mandatoryLectures, Set<Lecture> electiveLectures) {
     
 		OptionalMandatory optionalMandatory = OptionalMandatory.from(user);
-		int chooseNum = optionalMandatory.getChooseNUmber();
+		int chooseNum = optionalMandatory.getChooseNumber();
+		//전공과목Set에서 전공필수과목에 해당되는 과목들을 추출한다.
 		Set<Lecture> optionalMandatoryLectures = mandatoryLectures.stream().filter(
 			optionalMandatory.getOptionalMandatoryLectures()::contains).collect(Collectors.toSet());
+
 		Set<Lecture> remainingMandatoryLectures = new HashSet<>(optionalMandatoryLectures);
 		int count = 0;
 		for (TakenLecture takenLecture : takenLectureInventory.getTakenLectures()) {
