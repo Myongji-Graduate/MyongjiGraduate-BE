114a31518a60607ff69643d86dcb9ee7e5da14da
5uhwann <5uhwann27@gmail.com>
2024-01-23 23:15:52 +0900

    refactor: warning 제거

--- src/main/java/com/plzgraduate/myongjigraduatebe/parsing/domain/ParsingInformation.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/parsing/domain/ParsingInformation.java
@@ -1,9 +1,7 @@
 package com.plzgraduate.myongjigraduatebe.parsing.domain;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.Semester;
