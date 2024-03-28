e185d3a4c3a365319f17611cd96e9ebfad5d431f
5uhwann <5uhwann27@gmail.com>
2024-03-28 15:09:50 +0900

    feat: CompletedCreditRepository - 이수 학점 조회 구현

---
+++ src/test/java/com/plzgraduate/myongjigraduatebe/completedcredit/infrastructure/persistence/repository/CompletedCreditRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.plzgraduate.myongjigraduatebe.completedcredit.infrastructure.persistence.repository;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.plzgraduate.myongjigraduatebe.completedcredit.infrastructure.persistence.entity.CompletedCreditJpaEntity;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory;
+import com.plzgraduate.myongjigraduatebe.support.PersistenceTestSupport;
+import com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.entity.UserJpaEntity;
+import com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.repository.UserRepository;
+
+class CompletedCreditRepositoryTest extends PersistenceTestSupport {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private CompletedCreditRepository completedCreditRepository;
+
+	@DisplayName("유저의 이수 학점을 조회 한다.")
+	@Test
+	void findAllByUserJpaEntity() {
+		//given
+		UserJpaEntity userJpaEntity = UserJpaEntity.builder()
+			.id(1L)
+			.authId("test")
+			.password("test")
+			.studentNumber("60191111").build();
+
+		CompletedCreditJpaEntity commonCultureCompletedCreditJpaEntity = CompletedCreditJpaEntity.builder()
+			.userJpaEntity(userJpaEntity)
+			.category(GraduationCategory.COMMON_CULTURE)
+			.totalCredit(10)
+			.takenCredit(10).build();
+
+		CompletedCreditJpaEntity coreCultureCompletedCreditJpaEntity = CompletedCreditJpaEntity.builder()
+			.userJpaEntity(userJpaEntity)
+			.category(GraduationCategory.CORE_CULTURE)
+			.totalCredit(10)
+			.takenCredit(10).build();
+
+		userRepository.save(userJpaEntity);
+		completedCreditRepository.saveAll(
+			List.of(commonCultureCompletedCreditJpaEntity, coreCultureCompletedCreditJpaEntity));
+
+		//when
+		List<CompletedCreditJpaEntity> foundCompletedCredits = completedCreditRepository.findAllByUserJpaEntity(
+			userJpaEntity);
+
+		//then
+		Assertions.assertThat(foundCompletedCredits).hasSize(2)
+			.extracting("userJpaEntity.id")
+			.contains(1L);
+	}
+
+}
