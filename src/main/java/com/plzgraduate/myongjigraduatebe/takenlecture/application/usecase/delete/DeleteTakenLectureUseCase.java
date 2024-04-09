f788d4076b0979278ac0d4cc6a66d42dd41ca6ac
나경호 <gangho324@gmail.com>
2024-04-09 18:42:42 +0900

    feat: 수강과목 삭제 UseCase

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/takenlecture/application/usecase/delete/DeleteTakenLectureUseCase.java
@@ -0,0 +1,5 @@
+package com.plzgraduate.myongjigraduatebe.takenlecture.application.usecase.delete;
+
+public interface DeleteTakenLectureUseCase {
+	void deleteTakenLecture(Long deletedTakenLectureId);
+}
