c6208b5abd3666fe021c385f10f0f70ce370955a
나경호 <gangho324@gmail.com>
2024-04-09 18:40:08 +0900

    refactor: UpdateTakenLectureController update -> generate, delete로 분리

--- src/main/java/com/plzgraduate/myongjigraduatebe/takenlecture/api/UpdateTakenLectureController.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/takenlecture/api/UpdateTakenLectureController.java
@@ -2,14 +2,17 @@ package com.plzgraduate.myongjigraduatebe.takenlecture.api;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.plzgraduate.myongjigraduatebe.core.meta.LoginUser;
 import com.plzgraduate.myongjigraduatebe.core.meta.WebAdapter;
-import com.plzgraduate.myongjigraduatebe.takenlecture.api.dto.request.UpdateTakenLectureRequest;
-import com.plzgraduate.myongjigraduatebe.takenlecture.application.usecase.update.UpdateTakenLectureUseCase;
+import com.plzgraduate.myongjigraduatebe.takenlecture.api.dto.request.GenerateTakenLectureRequest;
+import com.plzgraduate.myongjigraduatebe.takenlecture.application.usecase.save.GenerateTakenLectureUseCase;
+import com.plzgraduate.myongjigraduatebe.takenlecture.application.usecase.delete.DeleteTakenLectureUseCase;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,13 +20,18 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/taken-lectures")
 @RequiredArgsConstructor
 public class UpdateTakenLectureController implements UpdateTakenLectureApiPresentation {
-	private final UpdateTakenLectureUseCase updateTakenLectureUseCase;
 
-	@PostMapping("/update")
-	public void updateTakenLectures(@LoginUser Long userId,
-		@Valid @RequestBody UpdateTakenLectureRequest updateTakenLectureRequest) {
-		updateTakenLectureUseCase.modifyTakenLecture(userId, updateTakenLectureRequest.getDeletedTakenLectures(),
-			updateTakenLectureRequest.getAddedTakenLectures());
+	private final GenerateTakenLectureUseCase generateTakenLectureUseCase;
+	private final DeleteTakenLectureUseCase deleteTakenLectureUseCase;
+
+	@PostMapping()
+	public void generateTakenLecture(@LoginUser Long userId,
+		@Valid @RequestBody GenerateTakenLectureRequest generateTakenLectureRequest) {
+		generateTakenLectureUseCase.generateTakenLecture(userId, generateTakenLectureRequest.getLectureId());
 	}
 
+	@DeleteMapping("{taken-lecture-id}")
+	public void deleteTakenLecture(@Valid @PathVariable("taken-lecture-id") Long takenLectureId) {
+		deleteTakenLectureUseCase.deleteTakenLecture(takenLectureId);
+	}
 }
