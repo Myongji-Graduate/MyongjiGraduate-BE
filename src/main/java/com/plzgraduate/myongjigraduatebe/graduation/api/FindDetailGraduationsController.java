42fabc45f99098105a78f54c4b2838b9069dc206
5uhwann <5uhwann27@gmail.com>
2024-04-10 21:39:20 +0900

    feat: FindDetailGraduationsController 구현 - 공통교양

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/api/FindDetailGraduationsController.java
@@ -0,0 +1,33 @@
+package com.plzgraduate.myongjigraduatebe.graduation.api;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.plzgraduate.myongjigraduatebe.core.meta.LoginUser;
+import com.plzgraduate.myongjigraduatebe.core.meta.WebAdapter;
+import com.plzgraduate.myongjigraduatebe.graduation.api.dto.response.DetailGraduationResultResponse;
+import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CalculateSingleDetailGraduationUseCase;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory;
+
+import lombok.RequiredArgsConstructor;
+
+@WebAdapter
+@RequestMapping("/api/v1/graduations/details")
+@RequiredArgsConstructor
+public class FindDetailGraduationsController {
+
+	private final CalculateSingleDetailGraduationUseCase calculateSingleDetailGraduationUseCase;
+
+	@GetMapping
+	public DetailGraduationResultResponse getDetailGraduations(@LoginUser Long userId,
+		@RequestParam GraduationCategory graduationCategory) {
+		DetailGraduationResult detailGraduationResult = calculateSingleDetailGraduationUseCase.calculateSingleDetailGraduation(
+			userId, graduationCategory);
+		return DetailGraduationResultResponse.from(detailGraduationResult);
+	}
+}
