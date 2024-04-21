3dc9997c5a575a21ba408fc87492d5e2a0307d87
5uhwann <5uhwann27@gmail.com>
2024-04-21 19:18:43 +0900

    feat: CalculateDetailGraduationUseCases 구현

---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CalculateDualBasicAcademicalCultureDetailGraduationUseService.java
@@ -0,0 +1,27 @@
+package com.plzgraduate.myongjigraduatebe.graduation.application.service;
+
+import static com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory.DUAL_BASIC_ACADEMICAL_CULTURE;
+
+import org.springframework.stereotype.Service;
+
+import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CalculateDualBasicAcademicalCultureDetailGraduationUseCase;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationRequirement;
+import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLectureInventory;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
+
+@Service
+public class CalculateDualBasicAcademicalCultureDetailGraduationUseService implements
+	CalculateDualBasicAcademicalCultureDetailGraduationUseCase {
+	@Override
+	public boolean supports(GraduationCategory graduationCategory) {
+		return graduationCategory == DUAL_BASIC_ACADEMICAL_CULTURE;
+	}
+
+	@Override
+	public DetailGraduationResult calculateDetailGraduation(User user, TakenLectureInventory takenLectureInventory,
+		GraduationRequirement graduationRequirement) {
+		return null;
+	}
+}
---
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CalculateSubMajorDetailGraduationService.java
@@ -0,0 +1,26 @@
+package com.plzgraduate.myongjigraduatebe.graduation.application.service;
+
+import static com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory.*;
+
+import org.springframework.stereotype.Service;
+
+import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CalculateSubMajorDetailGraduationUseCase;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationRequirement;
+import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLectureInventory;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
+
+@Service
+public class CalculateSubMajorDetailGraduationService implements CalculateSubMajorDetailGraduationUseCase {
+	@Override
+	public boolean supports(GraduationCategory graduationCategory) {
+		return graduationCategory == SUB_MAJOR;
+	}
+
+	@Override
+	public DetailGraduationResult calculateDetailGraduation(User user, TakenLectureInventory takenLectureInventory,
+		GraduationRequirement graduationRequirement) {
+		return null;
+	}
+}
