6293f0d12b2a04e47944ace6c562d7bcdafcd1b6
5uhwann <5uhwann27@gmail.com>
2024-04-24 22:09:00 +0900

    feat: CalculateCommonCultureGraduationUseCase 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CalculateCommonCultureGraduationService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CalculateCommonCultureGraduationService.java
@@ -1,18 +1,31 @@
 package com.plzgraduate.myongjigraduatebe.graduation.application.service;
 
-import static com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory.COMMON_CULTURE;
+import static com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory.*;
+
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CalculateCommonCultureGraduationUseCase;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationRequirement;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.GraduationManager;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.commonculture.CommonCultureGraduationManager;
+import com.plzgraduate.myongjigraduatebe.lecture.application.port.FindCommonCulturePort;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.CommonCulture;
 import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLectureInventory;
 import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CalculateCommonCultureGraduationService implements CalculateCommonCultureGraduationUseCase {
+
+	private final FindCommonCulturePort findCommonCulturePort;
 
 	@Override
 	public boolean supports(GraduationCategory graduationCategory) {
@@ -22,7 +35,10 @@ public class CalculateCommonCultureGraduationService implements CalculateCommonC
 	@Override
 	public DetailGraduationResult calculateDetailGraduation(User user, TakenLectureInventory takenLectureInventory,
 		GraduationRequirement graduationRequirement) {
-		return null;
+		Set<CommonCulture> graduationCommonCultures = findCommonCulturePort.findCommonCulture(user);
+		GraduationManager<CommonCulture> commonCultureGraduationManager = new CommonCultureGraduationManager();
+		return commonCultureGraduationManager.createDetailGraduationResult(
+			user, takenLectureInventory, graduationCommonCultures, graduationRequirement.getCommonCultureCredit());
 	}
 
 }
