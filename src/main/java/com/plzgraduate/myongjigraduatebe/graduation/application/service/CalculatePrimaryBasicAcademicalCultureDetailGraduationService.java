968648dd828bc8dce21d794a22ccbae2b62cbaf2
5uhwann <5uhwann27@gmail.com>
2024-05-22 11:23:08 +0900

    feat: 주 학문기초교양 졸업계산 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CalculatePrimaryBasicAcademicalCultureDetailGraduationService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CalculatePrimaryBasicAcademicalCultureDetailGraduationService.java
@@ -2,19 +2,36 @@ package com.plzgraduate.myongjigraduatebe.graduation.application.service;
 
 import static com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory.PRIMARY_BASIC_ACADEMICAL_CULTURE;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import com.plzgraduate.myongjigraduatebe.core.meta.UseCase;
 import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CalculateDetailGraduationUseCase;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationCategory;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationRequirement;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.GraduationManager;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.basicacademicalculture.BasicAcademicalManager;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.basicacademicalculture.BusinessBasicAcademicalManager;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.basicacademicalculture.DefaultBasicAcademicalManager;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.service.basicacademicalculture.SocialScienceBasicAcademicManager;
+import com.plzgraduate.myongjigraduatebe.lecture.application.port.FindBasicAcademicalCulturePort;
+import com.plzgraduate.myongjigraduatebe.lecture.domain.model.BasicAcademicalCultureLecture;
 import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLectureInventory;
 import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
 
+import lombok.RequiredArgsConstructor;
+
 @UseCase
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CalculatePrimaryBasicAcademicalCultureDetailGraduationService
 	implements CalculateDetailGraduationUseCase {
+
+	private final FindBasicAcademicalCulturePort findBasicAcademicalCulturePort;
+
 	@Override
 	public boolean supports(GraduationCategory graduationCategory) {
 		return graduationCategory == PRIMARY_BASIC_ACADEMICAL_CULTURE;
@@ -23,6 +40,26 @@ public class CalculatePrimaryBasicAcademicalCultureDetailGraduationService
 	@Override
 	public DetailGraduationResult calculateDetailGraduation(User user, TakenLectureInventory takenLectureInventory,
 		GraduationRequirement graduationRequirement) {
-		return null;
+		Set<BasicAcademicalCultureLecture> graduationBasicAcademicalCultureLectures = findBasicAcademicalCulturePort.findBasicAcademicalCulture(
+			user);
+		GraduationManager<BasicAcademicalCultureLecture> basicAcademicalCultureGraduationManager = determineBasicAcademicalCultureGraduationManager(
+			user);
+		DetailGraduationResult detailGraduationResult = basicAcademicalCultureGraduationManager.createDetailGraduationResult(
+			user, takenLectureInventory, graduationBasicAcademicalCultureLectures,
+			graduationRequirement.getPrimaryBasicAcademicalCredit());
+		detailGraduationResult.assignGraduationCategory(PRIMARY_BASIC_ACADEMICAL_CULTURE);
+		return detailGraduationResult;
+	}
+
+	private GraduationManager<BasicAcademicalCultureLecture> determineBasicAcademicalCultureGraduationManager(
+		User user) {
+		List<BasicAcademicalManager> basicAcademicalManagers = List.of(
+			new BusinessBasicAcademicalManager(),
+			new SocialScienceBasicAcademicManager());
+
+		return basicAcademicalManagers.stream()
+			.filter(basicAcademicalManager -> basicAcademicalManager.isSatisfied(user.getPrimaryMajor()))
+			.findFirst()
+			.orElse(new DefaultBasicAcademicalManager());
 	}
 }
