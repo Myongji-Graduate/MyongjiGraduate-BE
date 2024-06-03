3966f4c74d0d05960eb7cf809a5348804f9c3f40
5uhwann <5uhwann27@gmail.com>
2024-06-04 03:16:39 +0900

    feat: 복수전공 - 수강 중복 학문기초교양 조회 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/application/port/FindBasicAcademicalCulturePort.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/application/port/FindBasicAcademicalCulturePort.java
@@ -3,8 +3,11 @@ package com.plzgraduate.myongjigraduatebe.lecture.application.port;
 import java.util.Set;
 
 import com.plzgraduate.myongjigraduatebe.lecture.domain.model.BasicAcademicalCultureLecture;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
 
 public interface FindBasicAcademicalCulturePort {
 
 	Set<BasicAcademicalCultureLecture> findBasicAcademicalCulture(String major);
+
+	Set<BasicAcademicalCultureLecture> findDuplicatedLecturesBetweenMajors(User user);
 }
--- src/test/java/com/plzgraduate/myongjigraduatebe/lecture/infrastructure/adapter/persistence/repository/BasicAcademicalCultureLectureRepositoryTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/lecture/infrastructure/adapter/persistence/repository/BasicAcademicalCultureLectureRepositoryTest.java
@@ -1,24 +1,32 @@
 package com.plzgraduate.myongjigraduatebe.lecture.infrastructure.adapter.persistence.repository;
 
+import static com.plzgraduate.myongjigraduatebe.user.domain.model.College.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.plzgraduate.myongjigraduatebe.lecture.infrastructure.adapter.persistence.entity.BasicAcademicalCultureLectureJpaEntity;
 import com.plzgraduate.myongjigraduatebe.lecture.infrastructure.adapter.persistence.entity.LectureJpaEntity;
-import com.plzgraduate.myongjigraduatebe.lecture.infrastructure.adapter.persistence.repository.BasicAcademicalCultureRepository;
-import com.plzgraduate.myongjigraduatebe.lecture.infrastructure.adapter.persistence.repository.LectureRepository;
 import com.plzgraduate.myongjigraduatebe.support.PersistenceTestSupport;
+import com.plzgraduate.myongjigraduatebe.takenlecture.infrastructure.adapter.persistence.TakenLectureRepository;
+import com.plzgraduate.myongjigraduatebe.takenlecture.infrastructure.adapter.persistence.entity.TakenLectureJpaEntity;
+import com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.entity.UserJpaEntity;
+import com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.repository.UserRepository;
 
 class BasicAcademicalCultureLectureRepositoryTest extends PersistenceTestSupport {
 
 	@Autowired
 	private LectureRepository lectureRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private TakenLectureRepository takenLectureRepository;
 	@Autowired
 	private BasicAcademicalCultureRepository basicAcademicalCultureRepository;
 
@@ -43,6 +51,55 @@ class BasicAcademicalCultureLectureRepositoryTest extends PersistenceTestSupport
 		//then
 		assertThat(basicAcademicalCultureJpaEntities).extracting("college")
 			.contains(college);
+	}
+
+	@DisplayName("유저가 수강한 과목 중 유저의 주전공, 복수전공 양쪽 모두에 해당하는 학문기초교양의 수를 조회한다.")
+	@Test
+	void findAllDuplicatedTakenCountByCollages() {
+		//given
+		UserJpaEntity userJpaEntity = userRepository.save(UserJpaEntity.builder()
+			.authId("test")
+			.password("test")
+			.studentNumber("12341234")
+			.major("응용소프트웨어전공")
+			.dualMajor("경영학과").build());
+
+		LectureJpaEntity lectureJpaEntityA = LectureJpaEntity.builder()
+			.lectureCode("TESTA").build();
+		LectureJpaEntity lectureJpaEntityB = LectureJpaEntity.builder()
+			.lectureCode("TESTB").build();
+		List<LectureJpaEntity> lectureJpaEntities = lectureRepository.saveAll(
+			List.of(lectureJpaEntityA, lectureJpaEntityB));
+
+		TakenLectureJpaEntity takenLectureJpaEntityA = TakenLectureJpaEntity.builder()
+			.user(userJpaEntity)
+			.lecture(lectureJpaEntities.get(0))
+			.build();
+		TakenLectureJpaEntity takenLectureJpaEntityB = TakenLectureJpaEntity.builder()
+			.user(userJpaEntity)
+			.lecture(lectureJpaEntities.get(1))
+			.build();
+		takenLectureRepository.saveAll(List.of(takenLectureJpaEntityA, takenLectureJpaEntityB));
+
+		BasicAcademicalCultureLectureJpaEntity ictBasicAcademicalCultureJpaEntity = BasicAcademicalCultureLectureJpaEntity.builder()
+			.lectureJpaEntity(lectureJpaEntities.get(0))
+			.college(ICT.getName()).build();
+		BasicAcademicalCultureLectureJpaEntity businessBasicAcademicalCultureJpaEntity = BasicAcademicalCultureLectureJpaEntity.builder()
+			.lectureJpaEntity(lectureJpaEntities.get(1))
+			.college(BUSINESS.getName()).build();
+		BasicAcademicalCultureLectureJpaEntity bothBasicAcademicalCultureJpaEntity = BasicAcademicalCultureLectureJpaEntity.builder()
+			.lectureJpaEntity(lectureJpaEntities.get(0))
+			.college(BUSINESS.getName()).build();
+		basicAcademicalCultureRepository.saveAll(
+			List.of(ictBasicAcademicalCultureJpaEntity, businessBasicAcademicalCultureJpaEntity,
+				bothBasicAcademicalCultureJpaEntity));
+
+		//when
+		List<BasicAcademicalCultureLectureJpaEntity> duplicatedTakenBasicAcademicalCultures = basicAcademicalCultureRepository.findAllDuplicatedTakenByCollages(
+			userJpaEntity.getId(), ICT.getName(), BUSINESS.getName());
+
+		//then
+		assertThat(duplicatedTakenBasicAcademicalCultures).hasSize(1);
 	}
 
 	private List<BasicAcademicalCultureLectureJpaEntity> createBasicAcademicalCultures(
