3cbbc3fea569b6c22ec712d80a57c2a167de504c
5uhwann <5uhwann27@gmail.com>
2024-06-30 16:28:58 +0900

    feat: deleteAllByUserJpaEntity 구현

--- src/test/java/com/plzgraduate/myongjigraduatebe/completedcredit/infrastructure/persistence/repository/CompletedCreditRepositoryTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/completedcredit/infrastructure/persistence/repository/CompletedCreditRepositoryTest.java
@@ -1,8 +1,9 @@
 package com.plzgraduate.myongjigraduatebe.completedcredit.infrastructure.persistence.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,9 +51,43 @@ class CompletedCreditRepositoryTest extends PersistenceTestSupport {
 			userJpaEntity);
 
 		//then
-		Assertions.assertThat(foundCompletedCredits).hasSize(2)
+		assertThat(foundCompletedCredits).hasSize(2)
 			.extracting("userJpaEntity.authId")
 			.contains(userJpaEntity.getAuthId());
+	}
+
+	@DisplayName("유저의 모든 이수학점 내역을 삭제한다.")
+	@Test
+	void deleteAllByUserJpaEntity() {
+	    //given
+		UserJpaEntity userJpaEntity = UserJpaEntity.builder()
+			.authId("test1234")
+			.password("test")
+			.studentNumber("60191112").build();
+		userRepository.save(userJpaEntity);
+
+		CompletedCreditJpaEntity commonCultureCompletedCreditJpaEntity = CompletedCreditJpaEntity.builder()
+			.userJpaEntity(userJpaEntity)
+			.graduationCategory(GraduationCategory.COMMON_CULTURE)
+			.totalCredit(10)
+			.takenCredit(10).build();
+
+		CompletedCreditJpaEntity coreCultureCompletedCreditJpaEntity = CompletedCreditJpaEntity.builder()
+			.userJpaEntity(userJpaEntity)
+			.graduationCategory(GraduationCategory.CORE_CULTURE)
+			.totalCredit(10)
+			.takenCredit(10).build();
+
+		completedCreditRepository.saveAll(
+			List.of(commonCultureCompletedCreditJpaEntity, coreCultureCompletedCreditJpaEntity));
+
+	    //when
+		completedCreditRepository.deleteAllByUserJpaEntity(userJpaEntity);
+
+	    //then
+		List<CompletedCreditJpaEntity> foundCompletedCredits = completedCreditRepository.findAllByUserJpaEntity(
+			userJpaEntity);
+	    assertThat(foundCompletedCredits).isEmpty();
 	}
 
 }
