caf88aa528fd8ba595ffb0c6f58801bbcb78ae3a
k-kbk <kkbk0077@gmail.com>
2024-12-07 02:51:01 +0900

    feat: 비로그인 졸업 요건 검사 API 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/api/CheckGraduationRequirementApiPresentation.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/api/CheckGraduationRequirementApiPresentation.java
@@ -1,7 +1,7 @@
 package com.plzgraduate.myongjigraduatebe.graduation.api;
 
 import com.plzgraduate.myongjigraduatebe.graduation.api.dto.request.CheckGraduationRequirementRequest;
-import com.plzgraduate.myongjigraduatebe.graduation.api.dto.response.DetailGraduationResultResponse;
+import com.plzgraduate.myongjigraduatebe.graduation.api.dto.response.CheckGraduationRequirementResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "CheckGraduationRequirement", description = "로그인 없이 졸업 요건을 검사하는 API")
 public interface CheckGraduationRequirementApiPresentation {
 
-	DetailGraduationResultResponse checkGraduationRequirement(
+	CheckGraduationRequirementResponse checkGraduationRequirement(
 		@Valid @RequestBody CheckGraduationRequirementRequest checkGraduationRequirementRequest
 	);
 }
--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/api/CheckGraduationRequirementController.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/api/CheckGraduationRequirementController.java
@@ -2,9 +2,16 @@ package com.plzgraduate.myongjigraduatebe.graduation.api;
 
 import com.plzgraduate.myongjigraduatebe.core.meta.WebAdapter;
 import com.plzgraduate.myongjigraduatebe.graduation.api.dto.request.CheckGraduationRequirementRequest;
-import com.plzgraduate.myongjigraduatebe.graduation.api.dto.response.DetailGraduationResultResponse;
+import com.plzgraduate.myongjigraduatebe.graduation.api.dto.response.CheckGraduationRequirementResponse;
+import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CheckGraduationRequirementUseCase;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationResult;
+import com.plzgraduate.myongjigraduatebe.parsing.application.usecase.ParsingAnonymousUseCase;
+import com.plzgraduate.myongjigraduatebe.parsing.application.usecase.dto.ParsingAnonymousDto;
+import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -13,10 +20,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class CheckGraduationRequirementController implements CheckGraduationRequirementApiPresentation {
 
+	private final ParsingAnonymousUseCase parsingAnonymousUseCase;
+	private final CheckGraduationRequirementUseCase checkGraduationRequirementUseCase;
+
 	@PostMapping
-	public DetailGraduationResultResponse checkGraduationRequirement(
-		CheckGraduationRequirementRequest checkGraduationRequirementRequest
+	public CheckGraduationRequirementResponse checkGraduationRequirement(
+		@Valid @RequestBody CheckGraduationRequirementRequest checkGraduationRequirementRequest
 	) {
-		return null;
+		ParsingAnonymousDto parsingAnonymousDto = parsingAnonymousUseCase.parseAnonymous(
+			checkGraduationRequirementRequest.getEngLv(),
+			checkGraduationRequirementRequest.getParsingText()
+		);
+		User anonymous = parsingAnonymousDto.getAnonymous();
+		GraduationResult graduationResult = checkGraduationRequirementUseCase.checkGraduationRequirement(
+			anonymous,
+			parsingAnonymousDto.getTakenLectureInventory()
+		);
+
+		return new CheckGraduationRequirementResponse(
+			anonymous,
+			graduationResult
+		);
 	}
 }
