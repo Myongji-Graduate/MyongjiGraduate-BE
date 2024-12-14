a4bab8b5919453f17340a0c1bed850520a9a1f3b
k-kbk <kkbk0077@gmail.com>
2024-12-14 20:22:19 +0900

    feat: 비로그인 졸업 요건 검사 구현

--- src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CheckGraduationRequirementService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/graduation/application/service/CheckGraduationRequirementService.java
@@ -2,9 +2,13 @@ package com.plzgraduate.myongjigraduatebe.graduation.application.service;
 
 import com.plzgraduate.myongjigraduatebe.core.meta.UseCase;
 import com.plzgraduate.myongjigraduatebe.graduation.application.usecase.CheckGraduationRequirementUseCase;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.ChapelResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.DetailGraduationResult;
+import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationRequirement;
 import com.plzgraduate.myongjigraduatebe.graduation.domain.model.GraduationResult;
 import com.plzgraduate.myongjigraduatebe.takenlecture.domain.model.TakenLectureInventory;
 import com.plzgraduate.myongjigraduatebe.user.domain.model.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
@@ -18,6 +22,28 @@ public class CheckGraduationRequirementService implements CheckGraduationRequire
 		User anonymous,
 		TakenLectureInventory takenLectureInventory
 	) {
-		return null;
+
+		GraduationRequirement graduationRequirement =
+			calculateGraduationService.determineGraduationRequirement(anonymous);
+
+		ChapelResult chapelResult =
+			calculateGraduationService.generateChapelResult(takenLectureInventory);
+
+		List<DetailGraduationResult> detailGraduationResults = calculateGraduationService.generateDetailGraduationResults(
+			anonymous,
+			takenLectureInventory,
+			graduationRequirement
+		);
+
+		GraduationResult graduationResult = calculateGraduationService.generateGraduationResult(
+			chapelResult,
+			detailGraduationResults,
+			takenLectureInventory,
+			graduationRequirement
+		);
+
+		calculateGraduationService.handleDuplicatedTakenCredit(anonymous, graduationResult);
+
+		return graduationResult;
 	}
 }
