a0d5394757def17db084ce51729f4ee72ee5d24b
JinHyeon <tiemo0708@naver.com>
2025-09-16 15:50:03 +0900

    chore: 사용하지 않는 import 문 제거

--- src/test/java/com/plzgraduate/myongjigraduatebe/timetable/application/service/UserTimetableServiceTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/timetable/application/service/UserTimetableServiceTest.java
@@ -16,7 +16,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
--- src/test/java/com/plzgraduate/myongjigraduatebe/timetable/infrastructure/adapter/persistence/UserTimetableAdapterTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/timetable/infrastructure/adapter/persistence/UserTimetableAdapterTest.java
@@ -18,7 +18,6 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
