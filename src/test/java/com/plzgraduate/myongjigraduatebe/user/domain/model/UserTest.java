efe851a911ff3765a61a0138e7d34da356007173
JinHyeon <tiemo0708@naver.com>
2025-11-03 07:08:33 +0900

    test: user 인자 추가에 따른 테스트 코드 추가

--- src/test/java/com/plzgraduate/myongjigraduatebe/user/domain/model/UserTest.java
+++ src/test/java/com/plzgraduate/myongjigraduatebe/user/domain/model/UserTest.java
@@ -57,7 +57,7 @@ class UserTest {
 	@Test
 	void updateStudentInformation() {
 		//given //when
-		user.updateStudentInformation("테스터2", "경영학전공", null, null,  null, StudentCategory.CHANGE_MAJOR, null, null, 134,
+		user.updateStudentInformation("테스터2", "경영학전공", null, null, null, null, StudentCategory.CHANGE_MAJOR, null, null, 134,
 			120.5, true);
 		//then
 		assertThat(user)
