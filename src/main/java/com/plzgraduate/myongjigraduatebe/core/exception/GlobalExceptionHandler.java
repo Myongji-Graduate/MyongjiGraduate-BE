8b62e56877cff697d636ecd9268944efca495e89
baaamk <brandon55321@gmail.com>
2025-11-18 20:47:17 +0900

    refactor: 미사용 import 제거

--- src/main/java/com/plzgraduate/myongjigraduatebe/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/core/exception/GlobalExceptionHandler.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 
 @Slf4j
 @RestControllerAdvice
