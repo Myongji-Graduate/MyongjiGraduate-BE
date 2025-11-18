752d64345053bc18a046462cf384af59161961c9
baaamk <brandon55321@gmail.com>
2025-11-18 20:35:48 +0900

    feat: 빈 리스트 대신 에러 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/core/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
