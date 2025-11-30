235917eb9100fdb076408d374e61bd88bc6b59b4
baaamk <brandon55321@gmail.com>
2025-12-01 04:59:48 +0900

    refactor: 사용하지 않는 import 정리

--- src/main/java/com/plzgraduate/myongjigraduatebe/lecture/application/service/PopularLecturesService.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/lecture/application/service/PopularLecturesService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
