6eafd0676b868ddb61eb5682937a24aa585aa7b0
JinHyeon <tiemo0708@naver.com>
2026-01-04 15:46:57 +0900

    feat: ParsingTextRepository에 실패 조회 메서드 추가

--- src/main/java/com/plzgraduate/myongjigraduatebe/parsing/infrastructure/adapter/persistence/repository/ParsingTextRepository.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/parsing/infrastructure/adapter/persistence/repository/ParsingTextRepository.java
@@ -1,8 +1,10 @@
 package com.plzgraduate.myongjigraduatebe.parsing.infrastructure.adapter.persistence.repository;
 
+import com.plzgraduate.myongjigraduatebe.parsing.domain.FailureReason;
 import com.plzgraduate.myongjigraduatebe.parsing.domain.ParsingResult;
 import com.plzgraduate.myongjigraduatebe.parsing.infrastructure.adapter.persistence.entity.ParsingTextHistoryJpaEntity;
 import com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.entity.UserJpaEntity;
+import java.time.Instant;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +13,13 @@ public interface ParsingTextRepository extends JpaRepository<ParsingTextHistoryJ
 	void deleteAllByUser(UserJpaEntity user);
 
 	List<ParsingTextHistoryJpaEntity> findByParsingResultAndFailureReasonIsNull(ParsingResult parsingResult);
+
+	List<ParsingTextHistoryJpaEntity> findByParsingResult(ParsingResult parsingResult);
+
+	List<ParsingTextHistoryJpaEntity> findByFailureReasonIsNull();
+
+	List<ParsingTextHistoryJpaEntity> findByFailureReason(FailureReason failureReason);
+
+	List<ParsingTextHistoryJpaEntity> findByCreatedAtBetween(Instant startDate, Instant endDate);
 
 }
