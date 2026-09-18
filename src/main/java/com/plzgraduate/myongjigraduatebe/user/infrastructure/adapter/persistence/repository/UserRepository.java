5c6732032271ce573800fe2635b004ea2ae574be
JinHyeon <tiemo0708@naver.com>
2026-09-18 15:16:55 +0900

    [DEV-115] 졸업 학점 동시 조회 충돌 수정

--- src/main/java/com/plzgraduate/myongjigraduatebe/user/infrastructure/adapter/persistence/repository/UserRepository.java
+++ src/main/java/com/plzgraduate/myongjigraduatebe/user/infrastructure/adapter/persistence/repository/UserRepository.java
@@ -1,10 +1,18 @@
 package com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.repository;
 
 import com.plzgraduate.myongjigraduatebe.user.infrastructure.adapter.persistence.entity.UserJpaEntity;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserJpaEntity, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select u from UserJpaEntity u where u.id = :id")
+    Optional<UserJpaEntity> findByIdForUpdate(@Param("id") Long id);
 
 	Optional<UserJpaEntity> findByAuthId(String authId);
 
